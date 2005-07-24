b55ce2a1e129013ec30ebb4fc62edf886289c591
ejner <ejner@1de841de-8530-0410-b3ab-d372fa6ee214>
2005-07-24 09:01:42 +0000

    test functions and data

---
+++ trunk/tests/playerdatabase/main.cpp
@@ -0,0 +1,121 @@
+// main() to convert a scid ratings.ssp to our player db format,
+// and test the content of the resulting player database.
+//
+// To use it, change the parameters to
+// DatabaseConversion::PlayerDatabaseFromScidRatings()
+// from "~ejner/" to point at your actual directory and files.
+
+
+#include <iostream>
+#include "databaseconversion.h"
+
+
+void testPlayerDatabaseConversion(){
+  QString db_name = "/home/ejner/chessX/players";
+
+  int x = DatabaseConversion::PlayerDatabaseFromScidRatings("/home/ejner/chessX/ratings.ssp",db_name,"/home/ejner/chessX/polgar.jpg"); // the jpg is attached as photo to the first player in ratings.ssp
+  if (x!=0){
+    std::cout << "conversion failed\n";
+    return;
+  }
+
+// query the converted database
+
+  PlayerDatabase pdb;
+  if (pdb.open(db_name)) {
+    std::cout << "opened " << db_name << "\n";
+  }
+  else{
+    std::cout << "failure opening " << db_name << "\n";
+    return;
+  }
+
+  std::cout << "number of players = " << pdb.numberOfPlayers() <<"\n"; 
+  QStringList sl = pdb.playerNames();
+
+//  for (int i=0; i<sl.size()&&i<5; i++ ) {
+//    std::cout << sl[i] << "\n";
+//  }
+//  int i=0;
+//  for ( QStringList::Iterator it = sl.begin(); it != sl.end(); ++it ) {
+//    std::cout << *it << "\n";
+    //i++;
+//  }
+
+  QString d1 = pdb.dateOfBirth("Aaberg, Anton");
+  std::cout << "Aaberg, Anton birthdate= " << d1 << "\n";
+  QString s = pdb.country("Aaberg, Anton");
+  std::cout << "Aaberg, Anton country= " << s << "\n";
+  s = pdb.title("Aaberg, Anton");
+  std::cout << "Aaberg, Anton title= " << s << "\n";
+
+  d1 = pdb.dateOfBirth("Aaron, Manuel");
+  std::cout << "Aaron, Manuel birthdate= " << d1 << "\n";
+  s = pdb.country("Aaron, Manuel");
+  std::cout << "Aaron, Manuel country= " << s << "\n";
+  s = pdb.title("Aaron, Manuel");
+  std::cout << "Aaron, Manuel title= " << s << "\n";
+
+  int elo = pdb.elo("Aaberg, Anton",QDate(2000,5,5));
+  std::cout << "Aaberg, Anton elo on 2000/5/5= " << elo << "\n";
+  elo = pdb.elo("Aaron, Manuel",QDate(2000,5,5));
+  std::cout << "Aaron, Manuel elo on 2000/5/5= " << elo << "\n";
+
+  QString bio = pdb.biography("Aaberg, Anton");
+  if (bio.isNull())
+    std::cout << "Aaberg, Anton bio= null " << "\n";
+  else
+    std::cout << "Aaberg, Anton bio= " << bio << "\n";
+
+  bool bl = pdb.exists("BYE");
+  if (bl)
+    std::cout << "player BYE exists \n";
+  else
+    std::cout << "player BYE does not exist \n";
+
+  d1 = pdb.dateOfBirth("Azmaiparashvili, Zurab");
+  std::cout << "Azmaiparashvili, Zurab birthdate= " << d1 << "\n";
+  d1 = pdb.dateOfBirth("Babaev, Rashad");
+  std::cout << "Babaev, Rashad birthdate= " << d1 << "\n";
+  d1 = pdb.dateOfBirth("Zysk, Robert");
+  std::cout << "Zysk, Robert birthdate= " << d1 << "\n";
+  d1 = pdb.country("Zysk, Robert");
+  std::cout << "Zysk, Robert country= " << d1 << "\n";
+
+  d1 = pdb.dateOfBirth("Rudolf, Michal");
+  std::cout << "Rudolf, Michal birthdate= " << d1 << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(2000,10,5));
+  std::cout << "MR elo on 2000/10/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(2001,5,5));
+  std::cout << "MR elo on 2001/5/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(2001,10,5));
+  std::cout << "MR elo on 2001/10/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(2005,10,5));
+  std::cout << "MR elo on 2005/10/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(1995,10,5));
+  std::cout << "MR elo on 1995/10/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(1990,10,5));
+  std::cout << "MR elo on 1990/10/5= " << elo << "\n";
+  elo = pdb.elo("Rudolf, Michal",QDate(1997,1,1));
+  std::cout << "MR elo on 1997/1/1= " << elo << "\n";
+
+  QImage img = pdb.photo("'t Jong, Ewoud");
+  if (img.isNull())
+    std::cout << "'t Jong, Ewoud photo == null \n";
+  else
+    std::cout << "'t Jong, Ewoud photo != null \n";
+
+  pdb.close();
+  std::cout << "closed " << db_name << "\n";
+
+  std::cout << "end of testPlayerDatabaseConversion\n";
+}
+
+
+int main( ) 
+{
+testPlayerDatabaseConversion();
+  return 0;
+}
+
+
