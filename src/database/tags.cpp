9d7d21418831af0d126fdace67f1b026c727e294
ejner <ejner@1de841de-8530-0410-b3ab-d372fa6ee214>
2005-12-09 15:00:58 +0000

    Safety commit

--- src/database/tags.cpp
+++ src/database/tags.cpp
@@ -17,6 +17,7 @@
 
 #include <tags.h>
 //#include <iostream>
+//    std::cout <<  i  << "\n";
 
 static Q_UINT32 Magic = (Q_UINT32)0x00DDB0DE; // 'magic' number
 static Q_UINT32 Version = (Q_UINT32)100; // file format version
@@ -28,11 +29,13 @@ static char* knownTagNames[]={"Event","Site", "Date", "Round", "Name","Result",
 "Source"
 };
 
+Tags::Tags(const QString& fname){
+  m_fname = fname;
+  m_tags_file.setName(m_fname+Tagfile_suffix);
+  clear();
+}
 
-bool Tags::create(const QString& fname) {
-  m_tags_file.setName(fname+Tagfile_suffix);
-  if (m_tags_file.exists())
-     return false;
+void Tags::writeFile(){
 // set which QDataset version format to use
   if (Version==(Q_UINT32)100){
     m_tags_ds.setVersion(6);
@@ -40,25 +43,66 @@ bool Tags::create(const QString& fname) {
   else{
     m_tags_ds.setVersion(6);//default
   }
-  m_tags_file.open( IO_ReadWrite );
+  m_tags_file.open( IO_WriteOnly );
   m_tags_ds.setDevice(&m_tags_file);
   m_tags_ds << Magic;
   m_tags_ds << Version;
-  m_tags_file.flush();
-  m_allTags.clear();
-  for (uint i=0; i<Source; i++){
-    QValueVector<QString> first;
-    QMap<QString,int> second;
-    QPair <QValueVector<QString>, QMap <QString,int> > elm = qMakePair(first,second); 
-    m_allTags.insert(i,elm);
+  //store basic information for each tag type
+  m_tags_ds << (Q_UINT32)(m_allTags.count());
+  for(uint i=0; i<m_allTags.count(); i++){
+    m_tags_ds << (Q_UINT32)count(i);
+    if (isCustom(i)){
+      m_tags_ds << tagName(i).utf8();//write custom tag name
+//used flag
+      if (m_customTags_m.find(tagName(i)).data().second==true){
+        m_tags_ds << (Q_UINT8)1;
+      }
+      else{
+        m_tags_ds << (Q_UINT8)0;
+      }
+    }
   }
-  return true;
+  //write the tag value data
+  QString currentValue;
+  QString lastValue;
+  QByteArray ba;
+  bool first;
+  Q_UINT32 id;
+  Q_UINT32 freq;
+  Q_UINT8 prefixCount;
+  for(uint i=0; i<m_allTags.count(); i++){
+    first=true;
+    QMap <QString,QPair<Q_UINT32,Q_UINT32> > map = m_allTags[i].second;
+    QMap <QString,QPair<Q_UINT32,Q_UINT32> >::iterator map_itr;
+    for(map_itr = map.begin(); map_itr != map.end(); ++map_itr){
+      id = map_itr.data().first;
+      freq = map_itr.data().second;
+      m_tags_ds << id;
+      m_tags_ds << freq;
+      currentValue = map_itr.key();
+      if (first){
+        ba = currentValue.utf8();
+        m_tags_ds << ba;
+        first=false;
+      }
+      else{//use prefix coding, since values are sorted in the map
+        prefixCount=0;
+        while(lastValue.at(prefixCount)==currentValue.at(prefixCount)){
+          prefixCount++;
+        }
+        ba = currentValue.mid(prefixCount).utf8();
+        m_tags_ds << ba;
+        m_tags_ds << prefixCount;
+      }
+      lastValue = currentValue;
+    }
+  }
+  closeFile();
 }
 
-
-bool Tags::open(const QString& fname) {
-  m_tags_file.setName(fname+Tagfile_suffix);
-  if (!m_tags_file.open( IO_ReadWrite ))
+bool Tags::readFile() {
+  m_tags_file.setName(m_fname+Tagfile_suffix);
+  if (!m_tags_file.open( IO_ReadOnly ))
     return false;
   m_tags_ds.setDevice(&m_tags_file);
   Q_UINT32 tag_magic;
@@ -79,68 +123,88 @@ bool Tags::open(const QString& fname) {
     m_tags_file.close();
     return false;
   }
-  m_allTags.clear();
-  for (uint i=0; i<Source; i++){
-    QValueVector<QString> first;
-    QMap<QString,int> second;
-    QPair <QValueVector<QString>, QMap <QString,int> > elm = qMakePair(first,second); 
-    m_allTags.insert(i,elm);
-  }
-  Q_UINT8 tagId;
-  Q_UINT32 index;
-  QString name;
-  char* c_name;
-  while (!m_tags_ds.atEnd()){
-    m_tags_ds >> tagId;
-    m_tags_ds >> index;
-    m_tags_ds >> c_name;
-    name = c_name;//work with QString internally
-    if (index>0){// 0 means value was deleted
-      index--;
-      m_allTags[tagId].second.insert(name,(int)index);
-      m_allTags[tagId].first.push_back(name);
+  clear();
+//read the file
+  Q_UINT32 tagCount;
+  m_tags_ds >> tagCount;
+  Q_UINT32 valueCount;
+  Q_UINT8 prefixCount;
+  QString currentValue;
+  QString lastValue;
+  QByteArray ba;
+  bool first;
+  Q_UINT32 id;
+  Q_UINT32 freq;
+  Q_UINT8 used;
+  QValueVector<Q_UINT32> valueCounts;
+  for(uint i=0; i<tagCount; i++){
+    m_tags_ds >> valueCount;
+    valueCounts.push_back(valueCount);
+    if (isCustom(i)){
+      m_tags_ds >> ba;//read custom tag name
+      m_tags_ds >> used;
+      currentValue = QString::fromUtf8(ba);
+      bool b = (used!=0);
+      QPair<uint,bool> p(i,b);
+      m_customTags_m.insert(currentValue,p);
+      m_customTags_v.push_back(currentValue);
+//reserve an entry in values collections m_allTags
+      QValueVector<QString> first;
+      QMap<QString,QPair<Q_UINT32,Q_UINT32> > second;
+      QPair <QValueVector<QString>, QMap <QString,QPair<Q_UINT32,Q_UINT32> > > elm = qMakePair(first,second); 
+      m_allTags.insert(i,elm);
     }
-    else{
-      m_allTags[tagId].second.remove(name);
+  }
+  for(uint i=0; i<tagCount; i++){
+    m_allTags[i].first.resize(valueCounts[i]);
+    first=true;
+    for(uint j=0; j<valueCounts[i]; j++){
+      m_tags_ds >> id;
+      m_tags_ds >> freq;
+      m_tags_ds >> ba;
+      if (first){
+        currentValue = QString::fromUtf8(ba);
+        first = false;
+      }
+      else{
+        m_tags_ds >> prefixCount;
+        currentValue = lastValue.left(prefixCount).append(QString::fromUtf8(ba));
+      }
+      lastValue = currentValue;
+      m_allTags[i].first[id]=currentValue;
+      QPair<Q_UINT32,Q_UINT32> p(id,freq);
+      m_allTags[i].second.insert(currentValue,p);
     }
   }
+  closeFile();
   return true;
 }
 
-bool Tags::removeTagsFile(const QString& fname) {
-  m_tags_file.setName(fname+Tagfile_suffix);
+bool Tags::removeFile() {
   return m_tags_file.remove();
 }
 
-void Tags::close() {
-  m_tags_ds.unsetDevice();
-  m_tags_file.flush();
-  m_tags_file.close();
+QString Tags::value(const uint tagId, const int valueId) const{
+  return m_allTags[tagId].first[valueId];
 }
 
-QString Tags::value(uint tagId, int index){
-  QString s = m_allTags[tagId].first[index];
-   if (m_allTags[tagId].second.contains(s)){
-     return s;
-   }
-   else{
-     return "";
-   }
-}
-
-int Tags::index(uint tagId, const QString& value){
+int Tags::valueId(uint const tagId, const QString& value) const{
    if (m_allTags[tagId].second.contains(value)){
-     return m_allTags[tagId].second.find(value).data();
+     return m_allTags[tagId].second.find(value).data().first;
    }
    else{
      return -1;
    }
 }
 
-QValueVector<bool> Tags::find(const QString& pattern, QValueVector<QString>& vector, QMap<QString,int>& map){
+uint Tags::valueFrequency(const uint tagId, const int valueId) const{
+   return m_allTags[tagId].second.find(value(tagId,valueId)).data().second;
+}
+
+QValueVector<bool> Tags::find(const QString& pattern, QValueVector<QString>& vector, QMap<QString,QPair<Q_UINT32,Q_UINT32> >& map){
   QValueVector<bool> result;
   QValueVector<QString>::iterator it;
-  for( it = vector.begin(); it != vector.end(); ++it ){
+  for(it = vector.begin(); it != vector.end(); ++it){
     if (map.contains(*it) && (*it).startsWith(pattern)){
       result.push_back(true);
     }
@@ -151,10 +215,10 @@ QValueVector<bool> Tags::find(const QString& pattern, QValueVector<QString>& vec
   return result;
 }
 
-QValueVector<bool> Tags::find(const QRegExp& pattern, QValueVector<QString>& vector, QMap<QString,int>& map){
+QValueVector<bool> Tags::find(const QRegExp& pattern, QValueVector<QString>& vector, QMap<QString,QPair<Q_UINT32,Q_UINT32> >& map){
   QValueVector<bool> result;
   QValueVector<QString>::iterator it;
-  for( it = vector.begin(); it != vector.end(); ++it ){
+  for(it = vector.begin(); it != vector.end(); ++it){
     if (map.contains(*it) && pattern.search(*it)>=0){
       result.push_back(true);
     }
@@ -165,61 +229,177 @@ QValueVector<bool> Tags::find(const QRegExp& pattern, QValueVector<QString>& vec
   return result;
 }
 
-QValueVector<bool> Tags::find(uint tagId, const QString& pattern){
+QValueVector<bool> Tags::find(const uint tagId, const QString& pattern){
   return find(pattern, m_allTags[tagId].first, m_allTags[tagId].second);
 }
 
-QValueVector<bool> Tags::find(uint tagId, const QRegExp& pattern){
+QValueVector<bool> Tags::find(const uint tagId, const QRegExp& pattern){
   return find(pattern, m_allTags[tagId].first, m_allTags[tagId].second);
 }
 
-int Tags::add(uint tagId, const QString& value){
-  //  std::cout << "add called \n";
+int Tags::add(const uint tagId, const QString& value){
   if (m_allTags[tagId].second.contains(value)) {
-    return m_allTags[tagId].second.find(value).data() ;
+    (m_allTags[tagId].second.find(value).data().second)++;
+    return m_allTags[tagId].second.find(value).data().first;
   }
   else{
-    int idx = m_allTags[tagId].first.count();
+    Q_UINT32 idx = m_allTags[tagId].first.count();
     m_allTags[tagId].first.push_back(value);
-    m_allTags[tagId].second.insert(value,idx);
-    m_tags_ds << (Q_UINT8)tagId;
-    m_tags_ds << (Q_UINT32)idx+1;//0 is used for marking values as deleted
-    const char* c_name = value;//store as char* - QString serializes too heavily as UTF-16
-    m_tags_ds << c_name;
+    QPair<Q_UINT32,Q_UINT32> p(idx,1);
+    m_allTags[tagId].second.insert(value,p);
     return idx;
   }
 } 
 
-int Tags::count(uint tagId){
+int Tags::count(const uint tagId) const{
   return m_allTags[tagId].first.count();
 }
 
-void Tags::remove(uint tagId, int index){
-   QString value = m_allTags[tagId].first[index];
-   m_allTags[tagId].second.remove(value);
-   m_tags_ds << (Q_UINT8)tagId;
-   m_tags_ds << (Q_UINT32)0;//0 is used for marking deleted values
-   m_tags_ds << value;
+void Tags::remove(const uint tagId, const int valueId){
+   QString value = m_allTags[tagId].first[valueId];
+   if (m_allTags[tagId].second.find(value).data().second > 0){
+     (m_allTags[tagId].second.find(value).data().second)--;
+   }
 }
 
 void Tags::compact(){
-//not done yet
-/*  QValueVector<QString>::iterator it;
-  for( it = m_name_vector.begin(); it != m_name_vector.end(); ++it ){
-     if (!m_name_map.contains(*it)){
-       m_name_vector.erase(it);
-     }...
-  }*/
+//remove values with frequency==0
+// the index file has to be rewritten too.
 }
 
-void Tags::flushTagsFile(){
+bool Tags::isKnown(const uint tagId) const{
+  return tagId<=maxKnownTagIndex();
+}
+bool Tags::isKnownMandatory(const uint tagId) const{
+  return tagId<=Result;
+}
+bool Tags::isKnownOptional(const uint tagId) const{
+  return tagId<=maxKnownTagIndex() && tagId>Result;
+}
+bool Tags::isCustom(const uint tagId) const{
+  return tagId>maxKnownTagIndex();
+}
+
+int Tags::defineTag(const QString& tagName){
+  if (m_customTags_m.contains(tagName)){
+    return -1;
+  }
+  else{
+    Q_UINT16 newTagId = m_customTags_v.count()+maxKnownTagIndex()+1;
+    QPair<uint,bool> p(newTagId,true);
+    m_customTags_m.insert(tagName,p);
+    m_customTags_v.push_back(tagName);
+//reserve an entry in values collections m_allTags
+    QValueVector<QString> first;
+    QMap<QString,QPair<Q_UINT32,Q_UINT32> > second;
+    QPair <QValueVector<QString>, QMap <QString,QPair<Q_UINT32,Q_UINT32> > > elm = qMakePair(first,second); 
+    m_allTags.insert(newTagId,elm);
+    return newTagId;
+  }
+}
+
+bool Tags::unDefineTag(const QString& tagName){
+  if (m_customTags_m.contains(tagName)){
+    m_customTags_m.find(tagName).data().second = false;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+QStringList Tags::tagNames(const TagNameType type) {
+  QStringList qsl;
+  if (type==All || type==Known || type==KnownOptional || type==KnownMandatory){
+    for (uint i=0; i<=maxKnownTagIndex(); i++){
+      if (type==All || type==Known || (type==KnownOptional && isKnownOptional(i)) || (type==KnownMandatory && isKnownMandatory(i))){
+        qsl.append(knownTagNames[i]);
+      }
+    }
+  }
+  if (type==All || type==Custom){
+    QMap<QString,QPair<uint,bool> >::iterator it;
+    for( it = m_customTags_m.begin(); it != m_customTags_m.end(); ++it ){
+      if(it.data().second){//is used
+        qsl.append(it.key());
+      }
+    }
+  }
+  return qsl;
+}
+
+int Tags::tagId(const QString& tagName) const{
+  if (m_customTags_m.contains(tagName)){
+    if(m_customTags_m.find(tagName).data().second){//is used
+      return m_customTags_m.find(tagName).data().first;
+    }
+    else{
+      return -1;
+    }
+  }
+  else{
+    int i = knownTagIndex(tagName);
+    if (i>=0){
+      return i;
+    }
+    else{
+      return -1;
+    }
+  }
+}
+
+QString Tags::tagName(const uint tagId) const{
+  if (tagId>=minKnownTagIndex() && tagId<=maxKnownTagIndex()){
+    return knownTagNames[tagId];
+  }
+  else{
+    uint ix = tagId-maxKnownTagIndex()-1;
+    if (m_customTags_v.count()>ix){
+      QString tagName = m_customTags_v[ix];
+      if (m_customTags_m.contains(tagName)){
+        return tagName;
+      }
+      else{
+        return "";
+      }
+    }
+    else{
+      return "";
+    }
+  }
+}
+
+
+uint Tags::maxKnownTagIndex(){
+  return Source;
+}
+uint Tags::minKnownTagIndex(){
+  return Event;
+}
+
+int Tags::knownTagIndex(const QString& tagName){
+  for (uint i=0; i<=maxKnownTagIndex(); i++){
+    if (knownTagNames[i]==tagName){
+      return i;
+    }
+  }
+  return -1;
+}
+
+void Tags::clear(){
+  m_allTags.clear();
+  for (uint i=0; i<=maxKnownTagIndex(); i++){
+    QValueVector<QString> first;
+    QMap<QString,QPair<Q_UINT32,Q_UINT32> > second;
+    QPair <QValueVector<QString>, QMap <QString,QPair<Q_UINT32,Q_UINT32> > > elm = qMakePair(first,second); 
+    m_allTags.insert(i,elm);
+  }
+  m_customTags_m.clear();
+  m_customTags_v.clear();
+}
+
+void Tags::closeFile() {
+  m_tags_ds.unsetDevice();
   m_tags_file.flush();
-}
-
-int Tags::tagFileSize(){
-  return m_tags_file.at();
-}
-
-QString Tags::tagName(uint tagId){
-   return knownTagNames[tagId];
+  m_tags_file.close();
 }
--- tests/tags/main.cpp
+++ tests/tags/main.cpp
@@ -4,82 +4,57 @@
 
 static QTextStream out(stdout, IO_WriteOnly);
 
-/*
 void testTags(){
 QString fname = "/home/ejner/chessX/tagfiles/test";
-Tags tags;
-if (tags.removeTagsFile(fname))
-  out << "removed " << fname << "\n";
-else{
-  out << "error removing " << fname << "\n";
-  return;
-}
-if (tags.create(fname))
-  out << "created " << fname << "\n";
-else{
-  out << "error creating " << fname << "\n";
-  return;
-}
-
-tags.close();
-out << "closed " << fname << "\n";
-
-
-if (tags.open(fname))
-  out << "opened " << fname << "\n";
-else{
-  out << "error opening " << fname << "\n";
-  return;
-}
+Tags tags(fname);
 
 // add some values to known tags
-out << "added a tag: " << tags.add(Tags::PlayerName,"Karpov, A") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Kasparov, G") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Anand, W") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Holst, A") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Hansen, Soren Bech") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Rudolf, Michal") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Hoggarth, William") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Hopfgartner, Heinz R") << "\n";
-out << "added a tag: " << tags.add(Tags::PlayerName,"Roets, Marius") << "\n";
-out << "added a tag: " << tags.add(Tags::Event,"IECG-WC") << "\n";
-out << "added a tag: " << tags.add(Tags::Event,"IECF-WC") << "\n";
-out << "added a tag: " << tags.add(Tags::Site,"Scheveningen") << "\n";
-out << "added a tag: " << tags.add(Tags::Site,"Wijk-Aan-Zee") << "\n";
-out << "added a tag: " << tags.add(Tags::Site,"Los Angeles") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Karpov, A0") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Kasparov, G1") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Anand, W2") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Holst, A3") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Hansen, Søren Bech4") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Rudolf, Michal5") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Hoggarth, William6") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Hopfgartner, Heinz R7") << "\n";
+out << "added a value: " << tags.add(Tags::PlayerName,"Roets, Marius8") << "\n";
+out << "added a value: " << tags.add(Tags::Event,"IECG-WC0") << "\n";
+out << "added a value: " << tags.add(Tags::Event,"IECF-WC1") << "\n";
+out << "added a value: " << tags.add(Tags::Site,"Scheveningen0") << "\n";
+out << "added a value: " << tags.add(Tags::Site,"Wijk-Aan-Zee1") << "\n";
+out << "added a value: " << tags.add(Tags::Site,"Los Angeles2") << "\n";
 
-tags.close();
-tags.open(fname);
-
-out << "count tags of name type: " << tags.count(Tags::PlayerName) << "\n";
-out << "count tags of event type: " << tags.count(Tags::Event) << "\n";
-out << "count tags of site type: " << tags.count(Tags::Site) << "\n";
-
+tags.writeFile();
+tags.readFile();
 
 // look up by index
 for (int i=0; i<Tags::Source; i++){
+  out << "number of values of " << tags.tagName(i) << " tag type: " << tags.count(i) << "\n";
   for (int j=0; j<tags.count(i); j++){
     out << "Tag Name: " << tags.tagName(i) << ": value for index " << j << ": " << tags.value(i, j) << "\n";
   }
 }
 
 // search by value
-out << "index for Anand =" << tags.index(Tags::PlayerName,"Anand, W") << "\n";
+out << "index for Anand =" << tags.valueId(Tags::PlayerName,"Anand, W2") << "\n";
 
 // remove values
 tags.remove(Tags::PlayerName,2);
 out << "removed index 2\n";
+for (int i=0; i<tags.count(Tags::PlayerName); i++){
+  out << "playername tag value, index " << i << ": " << tags.value(Tags::PlayerName, i) << " has frequency: " << tags.valueFrequency(Tags::PlayerName, i) << "\n";
+}
 
 // match strings
 QValueVector<bool> vb = tags.find(Tags::PlayerName,"Ka");
-for (int i=0; i<vb.count(); i++){
+for (uint i=0; i<vb.count(); i++){
   if (vb[i])
     out << "index " << i << " matches prefix Ka \n";
   else
     out << "index " << i << " does not match prefix Ka \n";
 }
 vb = tags.find(Tags::Site,"L");
-for (int i=0; i<vb.count(); i++){
+for (uint i=0; i<vb.count(); i++){
   if (vb[i])
     out << "index " << i << " matches prefix L \n";
   else
@@ -87,54 +62,156 @@ for (int i=0; i<vb.count(); i++){
 }
 
 
-//TODO add some custom tags, and extract information on them
-
-
-out << "file size is " << tags.tagFileSize() << "\n";
-
-out << "closing " << fname << "\n";
-tags.close();
+// define some custom tags and values, and extract information on them
+QString s = "Difficulty";
+int id = tags.defineTag(s);
+out << s << " tag got index = " << id << "\n";
+s = "Opening Play";
+id = tags.defineTag(s);
+out << s << " tag got index = " << id << "\n";
+out << "All tag names: " << "\n";
+QStringList qsl = tags.tagNames();
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
 }
-*/
+out << "Custom tag names: " << "\n";
+qsl = tags.tagNames(Tags::Custom);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+
+tags.writeFile();
+tags.readFile();
+out << "after write/read...: " << "\n";
+
+out << "All tag names: " << "\n";
+qsl = tags.tagNames();
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+out << "Custom tag names: " << "\n";
+qsl = tags.tagNames(Tags::Custom);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+out << "Known tag names: " << "\n";
+qsl = tags.tagNames(Tags::Known);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+out << "Known optional tag names: " << "\n";
+qsl = tags.tagNames(Tags::KnownOptional);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+out << "Known mandatory tag names: " << "\n";
+qsl = tags.tagNames(Tags::KnownMandatory);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+
+s = "Opening Play";
+id = tags.tagId(s);
+out << "added a value: " << tags.add(id,"Nimzo") << "\n";
+out << "added a value: " << tags.add(id,"Nimzo") << "\n";
+out << "added a value: " << tags.add(id,"Bogo") << "\n";
+out << "added a value: " << tags.add(id,"Bogo") << "\n";
+out << "added a value: " << tags.add(id,"Bogo") << "\n";
+out << "added a value: " << tags.add(id,"HyperAcceleratedDragon") << "\n";
+out << "added a value: " << tags.add(id,"Tromp") << "\n";
+out << "added a value: " << tags.add(id,"DynamicEnglish") << "\n";
+out << "added a value: " << tags.add(id,"DynamicEnglish") << "\n";
+out << "added a value: " << tags.add(id,"DynamicEnglish") << "\n";
+tags.remove(id,tags.valueId(id,"DynamicEnglish"));
+out << "removed a value " << "\n";
+
+s = "Difficulty";
+id = tags.tagId(s);
+out << "added a value: " << tags.add(id,"Hard") << "\n";
+out << "added a value: " << tags.add(id,"Easy") << "\n";
+out << "added a value: " << tags.add(id,"Not so tough") << "\n";
+out << "added a value: " << tags.add(id,"A bit hard") << "\n";
+for (int i=0; i<tags.count(id); i++){
+  out << s << " tag value, index " << i << ": " << tags.value(id, i) << " has frequency: " << tags.valueFrequency(id, i) << "\n";
+}
+// remove values
+tags.remove(id,2);
+out << "removed index 2\n";
+for (int i=0; i<tags.count(id); i++){
+  out << s << " tag value, index " << i << ": " << tags.value(id, i) << " has frequency: " << tags.valueFrequency(id, i) << "\n";
+}
+tags.writeFile();
+tags.readFile();
+for (int i=0; i<tags.count(id); i++){
+  out << s << " tag value, index " << i << ": " << tags.value(id, i) << " has frequency: " << tags.valueFrequency(id, i) << "\n";
+}
+
+// undefine a custom tag
+s = "Difficulty";
+out << "undefining tag " << s << "\n";
+tags.unDefineTag(s);
+
+qsl = tags.tagNames(Tags::Custom);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "custom tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+
+tags.writeFile();
+tags.readFile();
+qsl = tags.tagNames(Tags::Custom);
+for ( QStringList::Iterator it = qsl.begin(); it != qsl.end(); ++it ) {
+   out << "custom tag name: " << *it << " has index = " << tags.tagId(*it) << "\n";
+}
+s = "Difficulty";
+id = tags.tagId(s);
+out << "Difficulty id: " << id << "\n";
+s = "Opening Play";
+id = tags.tagId(s);
+out << "Opening Play id: " << id << "\n";
+for (int i=0; i<tags.count(id); i++){
+  out << s << " tag value, index " << i << ": " << tags.value(id, i) << " has frequency: " << tags.valueFrequency(id, i) << "\n";
+}
+
+tags.writeFile();
+tags.readFile();
+for (int i=0; i<tags.count(id); i++){
+  out << s << " tag value, index " << i << ": " << tags.value(id, i) << " has frequency: " << tags.valueFrequency(id, i) << "\n";
+}
+
+
+}
+
+
 
 void testTags2(){
 // for performance testing
-QString fname = "/home/ejner/chessX/tagfiles/test";
-Tags tags;
-if (tags.removeTagsFile(fname))
-  out << "removed " << fname << "\n";
-else{
-  out << "error removing " << fname << "\n";
-  return;
-}
-if (tags.create(fname))
-  out << "created " << fname << "\n";
-else{
-  out << "error creating " << fname << "\n";
-  return;
-}
+// with DB2003-4, 419.00 players, 118.000 events, 131.000 sites
+// and 1.400 rounds, Scid namefile is 8,4MB
 
+QString fname = "/home/ejner/chessX/tagfiles/test";
+Tags tags(fname);
 
 QString s;
 // add 419.000 players
-for (int i=0; i<419000; i++){
+for (int i=0; i<418000; i++){
    s.setNum(i);
-   tags.add(Tags::PlayerName,"Karpov, A"+s);
+   tags.add(Tags::PlayerName,"Karpov,A"+s);
 }
 // add 118.000 events
-for (int i=0; i<118000; i++){
+
+for (int i=0; i<117000; i++){
    s.setNum(i);
-   tags.add(Tags::Event,"Scheveningen"+s);
+   tags.add(Tags::Event,"Berlin"+s);
 }
 // add 131.000 sites
-for (int i=0; i<131000; i++){
+for (int i=0; i<130000; i++){
    s.setNum(i);
-   tags.add(Tags::Site,"Wijk-Aan-Zee"+s);
+   tags.add(Tags::Site,"WijkAanZee"+s);
 }
 // add 1.400 rounds
-for (int i=0; i<1400; i++){
+for (int i=0; i<1300; i++){
    s.setNum(i);
-   tags.add(Tags::Round,"1."+s);
+   tags.add(Tags::Round,"1"+s);
 }
 
 out << "count tags of name type: " << tags.count(Tags::PlayerName) << "\n";
@@ -142,57 +219,20 @@ out << "count tags of event type: " << tags.count(Tags::Event) << "\n";
 out << "count tags of site type: " << tags.count(Tags::Site) << "\n";
 out << "count tags of round type: " << tags.count(Tags::Round) << "\n";
 
-/*
+tags.writeFile();
+//tags.readFile();
 // look up by index
-for (int i=0; i<Tags::Source; i++){
-  for (int j=0; j<tags.count(i); j++){
-    out << "Tag Name: " << tags.tagName(i) << ": value for index " << j << ": " << tags.value(i, j) << "\n";
-  }
+//out << "Tag Name: " << tags.tagName(Tags::PlayerName) << ": value for index " << 0 << ": " << tags.value(Tags::PlayerName, 0) << "\n";
+//out << "Tag Name: " << tags.tagName(Tags::PlayerName) << ": value for index " << 100000 << ": " << tags.value(Tags::PlayerName, 100000) << "\n";
+
 }
-
-// search by value
-out << "index for Anand =" << tags.index(Tags::PlayerName,"Anand, W") << "\n";
-
-// remove values
-tags.remove(Tags::PlayerName,2);
-out << "removed index 2\n";
-
-// match strings
-QValueVector<bool> vb = tags.find(Tags::PlayerName,"Ka");
-for (int i=0; i<vb.count(); i++){
-  if (vb[i])
-    out << "index " << i << " matches prefix Ka \n";
-  else
-    out << "index " << i << " does not match prefix Ka \n";
-}
-vb = tags.find(Tags::Site,"L");
-for (int i=0; i<vb.count(); i++){
-  if (vb[i])
-    out << "index " << i << " matches prefix L \n";
-  else
-    out << "index " << i << " does not match prefix L \n";
-}
-*/
-
-out << "file size is " << tags.tagFileSize() << "\n";
-
-// close and open to see performance
-tags.close();
-out << "closed " << fname << "\n";
-tags.open(fname);
-out << "opened " << fname << "\n";
-
-out << "closing " << fname << "\n";
-tags.close();
-}
-
 
 
 int main(int argv, char* argc[]) 
 {
   QApplication app(argv, argc);
-//  testTags();
-  testTags2();
+  testTags();
+  //testTags2();
   return 0;
 }
 
