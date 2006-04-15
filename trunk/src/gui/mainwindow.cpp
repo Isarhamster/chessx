edb2a7bdb3768045ce2a987592a027679ccfe712
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-04-15 00:50:50 +0000

    Add basic game/move view.

--- trunk/src/gui/mainwindow.cpp
+++ trunk/src/gui/mainwindow.cpp
@@ -86,6 +86,8 @@ MainWindow::MainWindow() : QMainWindow(0, "MainWindow", WDestructiveClose)
 
   /* Game */
   m_game = new Game;
+  m_game->setTag("White", "Botvinnik");
+  m_game->setTag("Black", "Polugaevsky");
 
   /* Layout */
   QFrame* frame = new QFrame(this);
@@ -97,11 +99,13 @@ MainWindow::MainWindow() : QMainWindow(0, "MainWindow", WDestructiveClose)
   layout->setRowStretch(0, 5);
   connect(this, SIGNAL(reconfigure()), m_boardView, SLOT(configure()));
   connect(m_boardView, SIGNAL(moveMade(Square, Square)), SLOT(slotMove(Square, Square)));
-  connect(m_boardView, SIGNAL(changed()), SLOT(slotUpdateMoveView()));
+  connect(m_boardView, SIGNAL(changed()), SLOT(slotMoveViewUpdate()));
 
   /* Move view */
  layout->addWidget(m_moveView = new QTextBrowser(frame), 1, 0);
+ connect(m_moveView, SIGNAL(linkClicked(const QString&)), SLOT(slotMoveViewLink(const QString&)));
  layout->setRowStretch(1, 1);
+ //m_moveView->styleSheet()->item("a")->setFontWeight(QFont::Bold);
  m_boardView->setBoard(m_game->board());
 
   /* Restoring layouts */
@@ -230,17 +234,20 @@ void MainWindow::slotMoveToEnd()
     m_boardView->setBoard(m_game->board());
 }
 
-void MainWindow::slotUpdateMoveView()
+void MainWindow::slotMoveViewUpdate()
 {
-  QString players = tr("Game %1: %2 %3, %4 %5").arg(1).arg(m_game->tag("White"))
-      .arg(m_game->tag("Result")).arg(m_game->tag("Black")).arg(m_game->tag("BlackElo"));
+  QString white = m_game->tag("White");
+  QString black = m_game->tag("Black");
+  QString players = tr("Game %1: <a href=\"player_%2\">%3</a> %4 - <a href=\"player_%5\">%6</a> %7").arg(1).arg(white).arg(white)
+      .arg(m_game->tag("Result")).arg(black).arg(black).arg(m_game->tag("BlackElo"));
   QString result = tr("%1(%2) %3").arg(m_game->tag("Result")).arg(m_game->plyCount() / 2)
       .arg(m_game->tag("ECO"));
   QString lastmove, nextmove;
   if (!m_game->atStart())
   {
     m_game->backward();
-    lastmove = QString("%1%2 %3").arg(m_game->ply() / 2 + 1).arg(m_game->ply() % 2 ? "..." : ".")
+    lastmove = QString("<a href=\"backward\">%1%2 %3</a>").arg(m_game->ply() / 2 + 1)
+        .arg(m_game->ply() % 2 ? "..." : ".")
         .arg(m_game->board().moveToSAN(m_game->move(0)));
     m_game->forward();
   }
@@ -248,12 +255,27 @@ void MainWindow::slotUpdateMoveView()
     lastmove = tr("(Start of game)");
   if (!m_game->atEnd())
   {
-    nextmove = QString("%1%2 %3").arg(m_game->ply() / 2 + 1).arg(m_game->ply() % 2 ? "..." : ".")
+    nextmove = QString("<a href=\"forward\">%1%2 %3</a>")
+        .arg(m_game->ply() / 2 + 1)
+        .arg(m_game->ply() % 2 ? "..." : ".")
         .arg(m_game->board().moveToSAN(m_game->move(0)));
   }
   else
     nextmove = tr("(End of game)");
   QString move = tr("Last move: %1 &nbsp; &nbsp; Next: %2").arg(lastmove).arg(nextmove);
   m_moveView->setText(QString("<qt>%1<br>%2<br>%3<br><qt>").arg(players).arg(result).arg(move));
+}
+
+void MainWindow::slotMoveViewLink(const QString& link)
+{
+  if (link == "backward")
+    slotMoveBackward();
+  else if (link == "forward")
+    slotMoveForward();
+  else if (link.startsWith("player_"))
+  {
+    m_playerDialog->findPlayers(link.section('_', 1));
+    m_playerDialog->show();
+  }
 }
 
