5fb59d5423eb3c2cca41302b8dd8b7427d21bbee
hhex <hhex@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-06-03 20:48:43 +0000

    Added test for simple executeSearch & fixed the change of signature to PgnDatabase::load( int , &Game ) in the testCopyGameIntoNewDB() unit test

--- trunk/tests/cppunit/pgndatabasetest.cpp
+++ trunk/tests/cppunit/pgndatabasetest.cpp
@@ -23,6 +23,8 @@ Unit tests for the PgnDatabase class.
 #include "pgndatabasetest.h"
 #include "pgndatabase.h"
 #include "game.h"
+#include "filter.h"
+#include "search.h"
 
 
 // Registers the fixture into the 'registry'
@@ -43,24 +45,43 @@ void PgnDatabaseTest::testCreateDatabase() {
 
 void PgnDatabaseTest::testCopyGameIntoNewDB() {
     PgnDatabase* db = new PgnDatabase( "./data/game1.pgn" );
-    Game game = db->load( 1 );
+    Game game;
+    bool success = db->load( 1, game );
     PgnDatabase* dbNew = new PgnDatabase( "./data/new.pgn" );
     dbNew->add
     ( game );
-    game = db->load( 0 );
-    dbNew->add( game );
+    success = db->load( 0, game );
+    dbNew->add
+    ( game );
     CPPUNIT_ASSERT_EQUAL( 2 , dbNew->count() );
-    CPPUNIT_FAIL( "Game index 1 - is the second game in the db! Is this OK?" );    
+    CPPUNIT_FAIL( "Game index 1 - is the second game in the db! Is this OK?" );
     delete dbNew, db;
 }
 
 void PgnDatabaseTest::testRemoveGame() {
     PgnDatabase* dbNew = new PgnDatabase( "./data/new.pgn" );
-    dbNew->remove( 1 );
+    dbNew->remove
+    ( 1 );
     CPPUNIT_ASSERT_EQUAL( 1 , dbNew->count() );
-    dbNew->remove( 0 );
+    dbNew->remove
+    ( 0 );
     CPPUNIT_ASSERT_EQUAL( 0 , dbNew->count() );
     delete dbNew;
 }
 
+void PgnDatabaseTest::testExecuteSearch() {
+    PgnDatabase* db = new PgnDatabase( "./data/game1.pgn" );
+    TagSearch search;
+    search.setTag( "Black" );
+    search.setValue( "Capablanca, Jose Raul" );
+    Filter filter = db->executeSearch( search );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Black search: ", 2, filter.count() );
+
+    TagSearch search1;
+    search1.setTag( "Result" );
+    search1.setValue( "1-0" );
+    Filter filter1 = db->executeSearch( search1 );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Result search: ", 1, filter1.count() );
+    delete db;
+}
 
