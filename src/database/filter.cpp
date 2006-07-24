77d440edfb93f5c389c9106a00041a6ea63ae538
whoggarth <whoggarth@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-07-24 21:39:16 +0000

    Changed filter api and implementation

--- src/database/filter.cpp
+++ src/database/filter.cpp
@@ -17,290 +17,297 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <filter.h>
+#include "filter.h"
 
-Filter::Filter(const int size, const Filter::WhichGames whichGames){
-   m_currentIndex = 0;
-   if (whichGames==AllGames){
-//Qt3 does not have QByteArray(size,ch)
-     m_byteArray = new QByteArray(size);
-     for (int i=0; i<size; i++){
-       (*m_byteArray)[i]=1;
-     }
-     m_count = size;
-     m_firstIndex = 0;
-     m_lastIndex = size-1;
-   }
-   else{
-     m_byteArray = new QByteArray(size);
-     for (int i=0; i<size; i++){
-       (*m_byteArray)[i]=0;
-     }
-     m_count = 0;
-     m_firstIndex = -1;
-     m_lastIndex = -1;
-   }
-}
-
-Filter::Filter(const Filter& filter){
-   m_byteArray = new QByteArray(filter.size());
-   m_count = filter.count();
-   m_firstIndex = filter.firstIndex();
-   m_lastIndex = filter.lastIndex();
-   m_currentIndex = filter.currentIndex();
-   for (int i=0; i<filter.size(); i++){
-     (*m_byteArray)[i]=filter.moveIndex(i);
-   }
-}
-
-Filter Filter::operator=(const Filter& filter)
+Filter::Filter(const int size, const Filter::WhichGames whichGames)
 {
-  m_count = filter.count();
-  m_firstIndex = filter.firstIndex();
-  m_lastIndex = filter.lastIndex();
-  m_currentIndex = filter.currentIndex();
-
-	QByteArray* byteArray = new QByteArray(filter.size());
-  for (int i=0; i<filter.size(); i++){
-    (*byteArray)[i]=filter.moveIndex(i);
-  }
-	delete m_byteArray;
-	m_byteArray = byteArray;
+	if(whichGames == AllGames) {
+		m_bitArray = new QBitArray(size);
+		m_bitArray->fill(true);
+		m_count = size;
+	} else {
+		m_bitArray = new QBitArray(size);
+		m_bitArray->fill(false);
+		m_count = 0;
+	}
 	
-	return *this;
+	m_lastNth = 0;
+	m_lastIndex = 0;
+}
+
+Filter::Filter(const Filter& filter)
+{
+	m_bitArray = new QBitArray(filter.bitArray());
+	m_count = filter.m_count;
+	m_lastNth = filter.m_lastNth;
+	m_lastIndex = filter.m_lastIndex;
 }
 
 Filter::Filter(const QBitArray& bitArray)
 {
-	m_count = bitArray.size();
-	m_currentIndex = 0;
-	
-	m_byteArray = new QByteArray(bitArray.size());
-	for(int i = 0; i < bitArray.size(); i++) {
-		if(bitArray[i]) {
-			if(m_firstIndex == -1) {
-				m_firstIndex = i;
-			}
-			m_lastIndex = i;
-		}
-		(*m_byteArray)[i] = bitArray[i];
+	m_count = 0;
+	m_bitArray = new QBitArray(bitArray);
+
+	for(int i = 0; i < (int)m_bitArray->size(); i++) {
+		m_count += m_bitArray->at(i);
 	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
+}
+
+Filter Filter::operator=(const Filter& filter)
+{
+	QBitArray* bitArray = new QBitArray(filter.bitArray());
+	delete m_bitArray;
+	m_bitArray = bitArray;
+	
+	m_count = filter.m_count;
+	m_lastNth = filter.m_lastNth;
+	m_lastIndex = filter.m_lastIndex;
+	
+	return *this;
 }
 
 Filter::~Filter()
 {
-	delete m_byteArray;
+	delete m_bitArray;
 }
 
-bool Filter::set(const unsigned int game, const unsigned int moveIndex){
-   if (game>(unsigned)m_byteArray->count() || moveIndex<1){
-     return false;
-   }
-   else{
-     if (m_byteArray->at(game) == 0){
-       m_count++;
-       if (m_count==1){
-         m_firstIndex = game;
-         m_lastIndex = game;
-       }
-       else{
-         m_firstIndex = MIN(m_firstIndex,(signed)game);
-         m_lastIndex = MAX(m_lastIndex,(signed)game);
-       }
-       m_currentIndex = game;
-     }
-     (*m_byteArray)[game]=moveIndex;
-     return true;
-   }
+void Filter::add(int game)
+{
+	if(game >= (int)m_bitArray->count()) {
+		return;
+	}
+
+	if(!m_bitArray->at(game)) {
+		//bit arrays are no longer explictly shared in Qt4
+		#if QT_VERSION < 0x040000
+		m_bitArray->detach();
+		#endif
+		
+		m_bitArray->setBit(game, true);
+		m_count++;
+		
+		if(game < m_lastNth) {
+			m_lastNth++;
+		}
+	}
 }
 
-bool Filter::remove(const unsigned int game){
-   if (game>(unsigned)m_byteArray->count()){
-     return false;
-   }
-   else{
-     if (m_byteArray->at(game) > 0){
-       m_count--;
-       (*m_byteArray)[game]=0;
-       if ((signed)game==m_firstIndex){
-         for(int i=game;i<(int)m_byteArray->count();i++){
-           if (m_byteArray->at(i) > 0){
-             m_firstIndex = i;
-             break;
-           }
-         }
-       }
-       if ((signed)game==m_lastIndex){
-         for(int i=game;i>=0;i--){
-           if (m_byteArray->at(i) > 0){
-             m_lastIndex = i;
-             break;
-           }
-         }
-       }
-     }
-     return true;
-   }
+void Filter::remove(int game)
+{
+	if(game >= (int)m_bitArray->count()) {
+		return;
+	}
+	
+  if(m_bitArray->at(game)) {
+		//bit arrays are no longer explictly shared in Qt4
+		#if QT_VERSION < 0x040000
+		m_bitArray->detach();
+		#endif
+		
+		m_bitArray->setBit(game, false);
+		m_count--;
+		
+		if(game < m_lastNth) {
+			m_lastNth--;
+		}	else if(game == m_lastNth) {
+			m_lastNth = 0;
+			m_lastIndex = 0;
+		}
+	}
 }
 
-int Filter::count() const{
-   return m_count;
+void Filter::set(int game, bool value)
+{
+	if(game >= (int)m_bitArray->count()) {
+		return;
+	}
+
+	if(m_bitArray->at(game) != game) {
+		//bit arrays are no longer explictly shared in Qt4
+		#if QT_VERSION < 0x040000
+		m_bitArray->detach();
+		#endif
+		
+		m_bitArray->setBit(game, value);
+		if(value) {
+			m_count++;
+			
+			if(game < m_lastNth) {
+				m_lastNth++;
+			}
+		} else {
+			m_count--;
+			
+			if(game < m_lastNth) {
+				m_lastNth--;
+			}	else if(game == m_lastNth) {
+				m_lastNth = 0;
+				m_lastIndex = 0;
+			}
+		}
+	}
 }
+
+void Filter::setAll(bool value)
+{
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	m_bitArray->detach();
+	#endif
+	
+	m_bitArray->fill(value);
+	m_count = value * (int)m_bitArray->size();
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
+}
+
+bool Filter::contains(const int game) const
+{
+	return m_bitArray->at(game);
+}
+
+int Filter::gameIndex(int nth) const
+{
+	if(nth >= m_count) {
+		return -1;
+	}
+	
+	int i;
+	int count;
+	
+	if(nth >= m_lastNth) {
+		i = m_lastIndex + 1;
+		count = m_lastNth;
+	} else {
+		i = 0;
+		count = 0;
+	}
+	
+	while(count < nth) {
+		count += m_bitArray->at(i);
+		i++;
+	}
+
+	m_lastNth = nth;
+	m_lastIndex = i - 1;
+	
+	return i - 1;
+}
+
+int Filter::count() const
+{
+	return m_count;
+}
+
 int Filter::size() const{
-   return m_byteArray->count();
+	return (int)m_bitArray->count();
 }
 
-bool Filter::contains(const int game) const{
-   return (m_byteArray->at(game) > 0);
-}
-int Filter::moveIndex(const int game) const{
-   return m_byteArray->at(game);
-}
-
-int Filter::currentIndex() const{
-   return m_currentIndex;
-}
-bool Filter::setCurrentIndex(const unsigned int index) {
-   if (index>=(unsigned)m_byteArray->count()||m_byteArray->at(index)==0){
-     return false;
-   }
-   else{
-     m_currentIndex = index;
-     return true;
-   }
-}
-int Filter::lastIndex() const{
-   return m_lastIndex;
-}
-int Filter::firstIndex() const{
-   return m_firstIndex;
+void Filter::resize(int size, Filter::WhichGames whichGames)
+{
+	delete m_bitArray;
+	
+	if(whichGames == AllGames) {
+		m_bitArray = new QBitArray(size);
+		m_bitArray->fill(true);
+		m_count = size;
+	} else {
+		m_bitArray = new QBitArray(size);
+		m_bitArray->fill(false);
+		m_count = 0;
+	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
 }
 
-void Filter::toFirst(){
-   m_currentIndex = m_firstIndex;
+void Filter::reverse()
+{
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	m_bitArray->detach();
+	#endif
+
+	m_count = m_bitArray->count() - m_count;
+
+	for(int i = 0; i < (int)m_bitArray->count(); i++) {
+		m_bitArray->toggleBit(i);
+	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
 }
 
-void Filter::toLast(){
-   m_currentIndex = m_lastIndex;
+void Filter::intersect(const Filter& filter)
+{
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	m_bitArray->detach();
+	#endif
+
+	m_count = 0;
+
+	for(int i = 0; i < (int)m_bitArray->count(); i++) {
+		m_bitArray->setBit(i, m_bitArray->at(i) && filter.m_bitArray->at(i));
+		m_count += m_bitArray->at(i);
+	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
 }
 
-int Filter::previousGame(const unsigned int index, const unsigned int offset) const{
-   if ((signed)index<=m_firstIndex||m_count==0||index>(unsigned)m_byteArray->count()){//there is no previous game
-     return -1;
-   }
-   if (offset==0){
-     return index;
-   }
-   int result=-1;
-   unsigned int count=0;
-   for (int i=index-1; i>=0; i--){
-     if (m_byteArray->at(i) > 0){
-       count++;
-       if (count==offset){
-         result=i;
-         break;
-       }
-     }
-   }
-   return result;
-}
-int Filter::nextGame(const int index, const int offset) const{
-   if (index>=m_lastIndex||m_count==0||index<-1||offset<0){//there is no next game
-     return -1;
-   }
-   if (offset==0){
-     return index;
-   }
-   int result=-1;
-   int count=0;
-   for (int i=index+1; i<(int)m_byteArray->count(); i++){
-     if (m_byteArray->at(i) > 0){
-       count++;
-       if (count==offset){
-         result=i;
-         break;
-       }
-     }
-   }
-   return result;
-}
-void Filter::reverse(){
-   m_count = m_byteArray->count() - m_count;
-   m_firstIndex = -1;
-   m_lastIndex = -1;
-   m_currentIndex = -1;
-   for (int i=0; i<(int)m_byteArray->count(); i++){
-     if (m_byteArray->at(i) == 0){
-       (*m_byteArray)[i]=1;
-       if (m_firstIndex==-1){
-         m_firstIndex = i;
-         m_lastIndex = i;
-         m_currentIndex = i;
-       }
-       else{
-         m_lastIndex = i;
-       }
-     }
-     else{
-       (*m_byteArray)[i]=0;
-     }
-   }
-}
-void Filter::intersect(const Filter& filter){
-   m_firstIndex = -1;
-   m_lastIndex = -1;
-   m_currentIndex = -1;
-   for (int i=0; i<(int)m_byteArray->count(); i++){
-     if (m_byteArray->at(i) > 0){
-       if (filter.contains(i)){
-         if (m_firstIndex==-1){
-           m_firstIndex = i;
-           m_lastIndex = i;
-           m_currentIndex = i;
-         }
-         else{
-           m_lastIndex = i;
-         }
-       }
-       else{
-         m_count--;
-         (*m_byteArray)[i]=0;
-       }
-     }
-   }
-}
 void Filter::add(const Filter& filter){
-   m_firstIndex = MIN(m_firstIndex,filter.firstIndex());
-   m_lastIndex = MAX(m_lastIndex,filter.lastIndex());
-   for (int i=0; i<(int)m_byteArray->count(); i++){
-     if (m_byteArray->at(i) == 0){
-       if (filter.contains(i)){
-         m_count++;
-         (*m_byteArray)[i]=filter.moveIndex(i);
-       }
-     }
-   }
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	m_bitArray->detach();
+	#endif
+	
+	m_count = 0;
+
+	for(int i = 0; i < (int)m_bitArray->count(); i++) {
+		m_bitArray->setBit(i, m_bitArray->at(i) || filter.m_bitArray->at(i));
+		m_count += m_bitArray->at(i);
+	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
 }
-void Filter::remove(const Filter& filter){
-   m_firstIndex = -1;
-   m_lastIndex = -1;
-   m_currentIndex = -1;
-   for (int i=0; i<(int)m_byteArray->count(); i++){
-     if (m_byteArray->at(i) > 0){
-       if (filter.contains(i)){
-         m_count--;
-         (*m_byteArray)[i]=0;
-       }
-       else{
-         if (m_firstIndex==-1){
-           m_firstIndex = i;
-           m_lastIndex = i;
-           m_currentIndex = i;
-         }
-         else{
-           m_lastIndex = i;
-         }
-       }
-     }
-   }
+
+void Filter::remove(const Filter& filter)
+{
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	m_bitArray->detach();
+	#endif
+	
+	m_count = 0;
+
+	for(int i = 0; i < (int)m_bitArray->count(); i++) {
+		m_bitArray->setBit(i, m_bitArray->at(i) && ~filter.m_bitArray->at(i));
+		m_count += m_bitArray->at(i);
+	}
+	
+	m_lastNth = 0;
+	m_lastIndex = 0;
+}
+
+QBitArray Filter::asBitArray() const
+{
+	QBitArray copy = *m_bitArray;
+	
+	//bit arrays are no longer explictly shared in Qt4
+	#if QT_VERSION < 0x040000
+	copy->detach();
+	#endif
+	
+	return copy;
+}
+
+QBitArray Filter::bitArray() const
+{
+	return *m_bitArray;
 }
