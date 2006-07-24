401e2e696f143deca32eb7ea966e50c888449813
whoggarth <whoggarth@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-07-24 21:39:16 +0000

    Changed filter api and implementation

--- trunk/src/database/filter.h
+++ trunk/src/database/filter.h
@@ -21,115 +21,86 @@
 #define __FILTER_H__
 #include "common.h"
 #include <qbitarray.h>
-#include <qt34/qbytearray.h>
 
-//TODO
-/*
-* unit tests (when design is more stable - can copy from /tests/filter)
-* possible optimizations
-  * cache one value (but what do we know about calling sequence ?? )
-  * use QValueVector as representation in case of "sparse" filter
-* saving/loading to disk (dataset).
-*/
 
 /**
    The Filter class represents a set of games, typically found when searching a database.
 */
 
-
 class Filter
 {
 public:
-/**
-    flag for which games to include in filter
-*/
-        enum WhichGames{AllGames,NoGames};
-/** construct filter of given size
-*/
-	Filter(const int size, const WhichGames whichGames = AllGames);
-/** construct filter from another filter
-*/
+	/** flag for which games to include in filter */
+	enum WhichGames { AllGames, NoGames };
+	
+	/** construct filter of given size */
+	Filter(int size, WhichGames whichGames = AllGames);
+	
+	/** construct filter from another filter */
 	Filter(const Filter& filter);
-/** construct filter from a bit array
-*/
+
+	/** construct filter from a bit array */
 	Filter(const QBitArray& bitArray);
-/** assignment operator
-*/
-	Filter operator=(const Filter& game);
-/** destructor
-*/
+	
+	/** assignment operator */
+	Filter operator=(const Filter& filter);
+
+	/** destructor */
 	~Filter();
-/** include game in filter, at the given move
-*/
-        bool set(const unsigned int game, const unsigned int moveIndex);
-/** remove game from filter
-*/
-        bool remove(const unsigned int game);
-/** return number of games in the filter
-*/
-        int count() const;
-/** return the size of the filter
-*/
-        int size() const;
-/** return true iff the game is in the filter
-*/
-	bool contains(const int game) const;
-/** return the move index for a game that is in the filter
-    0 if game is not in the filter
-    1 means start position
-*/
-        int moveIndex(const int game) const;
-/** return index of current game in filter
-*/
-	int currentIndex() const;
-/** set index of current game in filter
-*/
-	bool setCurrentIndex(const unsigned int index);
-/** return index of first game in filter
-*/
-	int lastIndex() const;
-/** return index of first game in filter
-*/
-	int firstIndex() const;
-/** make first game in filter the current one
-*/
-	void toFirst();
-/** make last game in filter the current one
-*/
-	void toLast();
-/** return index of the offset'th next game in filter relative to startIndex
-*/
-	int nextGame(const int startIndex, const int offset) const;
-/** return index of the offset'th previous game in filter relative to endIndex
-*/
-	int previousGame(const unsigned int endIndex, const unsigned int offset) const;
-/** reverse the filter (complement set)
-*/
+	
+	/** include a game in the filter */
+	void add(int game);
+	
+	/** remove a game from the filter */
+	void remove(int game);
+	
+	/** sets the value for a game */
+	void set(int game, bool value);
+	
+	/** sets all games in the filter to the same value */
+	void setAll(bool value);
+
+	/** return true if the game is in the filter */
+	bool contains(int game) const;
+	
+	/** returns the index of nth game in the filter (zero based)*/ 
+	int gameIndex(int nth) const;
+
+	/** return number of games in the filter */
+	int count() const;
+	
+	/** return the size of the filter */
+	int size() const;
+	
+	/** resizes the filter to the specified size */
+	void resize(int size, Filter::WhichGames whichGames = AllGames);
+	
+	/** reverse the filter (complement set) */
 	void reverse();
-/** intersect filter with another filter
-*/
-        void intersect(const Filter& filter);
-/** add the games of another filter to the filter
-*/
+	
+	/** intersect filter with another filter */
+	void intersect(const Filter& filter);
+	
+	/** add the games of another filter to the filter */
 	void add(const Filter& filter);
-/** remove the games of another filter from the filter
-*/
+	
+	/** remove the games of another filter from the filter */
 	void remove(const Filter& filter);
+	
+	/** returns the filter as a bit array (returns copy in Qt3 due, implicitly shared in Qt4)*/
+	QBitArray asBitArray() const;
+	
+protected:
+	/** returns the filter as a bit array (explicitly shared in Qt3, implicitly shared in Qt4)*/
+	QBitArray bitArray() const;
 
-
-private:
-
-    QByteArray* m_byteArray;//array of chars
-                            // =='0' means game with that index is not in the filter
-                            // !='0' means game with that index is in the filter, and the value
-                            //       denotes the move index for the match
-    int m_count;//number of games in the filter
-    int m_currentIndex;//the actual position within the filter
-    int m_firstIndex;//the position of the first game that's in the filter
-    int m_lastIndex;//the position of the last game that's in the filter
+	int m_count;
+	QBitArray* m_bitArray;
+	
+	// for the speeding up of sequential access
+	mutable int m_lastNth;
+	mutable int m_lastIndex;
 };
 
-
 #endif
-
 
