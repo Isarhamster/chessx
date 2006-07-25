7232f49417d6ef101def87dd66ca1dcc64b10b4b
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-07-25 21:44:15 +0000

    Add Round field. Make column names translateable.

--- trunk/src/gui/gamelist.h
+++ trunk/src/gui/gamelist.h
@@ -28,7 +28,7 @@ class GameList : public QWidget
 {
   Q_OBJECT
 public:
-  enum Column {Index, White, Black, Event, Site, Date, Result, ECO, Length};
+  enum Column {Index, White, Black, Event, Site, Round, Date, Result, ECO, Length};
   GameList(QWidget* parent = 0, const char *name = 0);
   /** Set current database */
   void setDatabase(Database* database);
