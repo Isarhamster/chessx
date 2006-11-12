1676474dd986340913175f76e1f2e83baaa7b9a0
saidinwielder <saidinwielder@1de841de-8530-0410-b3ab-d372fa6ee214>
2006-11-12 19:07:00 +0000

    Index array for in memory headers

---
+++ trunk/src/database/index.cpp
@@ -0,0 +1,213 @@
+/***************************************************************************
+                          filename - description
+                             -------------------
+    begin                : 
+    copyright            : (C) 2006 Marius Roets <saidinwielder@sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "index.h"
+
+Index::Index()
+{
+   m_gameIndex.clear();
+}
+
+Q_UINT32 Index::add (IndexItem item)
+{
+   m_gameIndex.append(item);
+   return m_gameIndex.count()-1;
+}
+
+Q_UINT32 Index::add ()
+{
+   IndexItem item;
+   m_gameIndex.append(item);
+   return m_gameIndex.count()-1;
+}
+
+void Index::setBlackId(Q_UINT32 index, Q_UINT32 id)
+{
+   m_gameIndex[index].setBlackId(id);
+}
+void Index::setWhiteId(Q_UINT32 index, Q_UINT32 id)
+{
+   m_gameIndex[index].setWhiteId(id);
+}
+void Index::setEventId(Q_UINT32 index, Q_UINT32 id)
+{
+   m_gameIndex[index].setEventId(id);
+}
+void Index::setSiteId(Q_UINT32 index, Q_UINT32 id)
+{
+   m_gameIndex[index].setSiteId(id);
+}
+void Index::setWhiteElo(Q_UINT32 index, Q_UINT16 elo)
+{
+   m_gameIndex[index].setWhiteElo(elo);
+}
+void Index::setBlackElo(Q_UINT32 index, Q_UINT16 elo)
+{
+   m_gameIndex[index].setBlackElo(elo);
+}
+void Index::setEcoId(Q_UINT32 index, Q_UINT16 id)
+{
+   m_gameIndex[index].setEcoId(id);
+}
+void Index::setResult(Q_UINT32 index, Result result)
+{
+   m_gameIndex[index].setResult(result);
+}
+void Index::setRound(Q_UINT32 index, Q_UINT8 round)
+{
+   m_gameIndex[index].setRound(round);
+}
+void Index::setDate(Q_UINT32 index, const PartialDate& date)
+{
+   m_gameIndex[index].setDate(date);
+}
+
+
+IndexItem& Index::gameIndex(Q_UINT32 index)
+{
+   return m_gameIndex[index];
+}
+
+void Index::remove()
+{
+}
+
+bool Index::find (Q_UINT32 gameId, Search* search, Tags& tags)
+{
+   QString tagName;
+   QValueVector<bool> vb;
+   int tagId;
+   TagSearch* ts;
+   EloSearch* es;
+   DateSearch* ds;
+   switch (search->type()) {
+      case Search::TagSearch:
+         ts = static_cast<TagSearch*> (search);
+         tagName = ts->tag();
+         if ((tagName == "White") || (tagName == "Black")) {
+            tagName = "Name";
+         } else if ((tagName == "BlackElo") || (tagName == "WhiteElo")) {
+            tagName = "Elo";
+         }
+         tagId = tags.tagId(tagName);
+         if (tagId == -1) {
+            return false;
+         }
+         vb = tags.find(tagId,ts->value());
+         //m_gameIndex[gameId].
+         tagName = ts->tag();
+         if (tagName == "White") {
+            if (vb[m_gameIndex[gameId].whiteId()]) {
+               return true; 
+            }
+         } else if (tagName == "Black") {
+            if (vb[m_gameIndex[gameId].blackId()]) {
+               return true; 
+            }
+         } else if (tagName == "Site") {
+            if (vb[m_gameIndex[gameId].siteId()]) {
+               return true; 
+            }
+         } else if (tagName == "Event") {
+            if (vb[m_gameIndex[gameId].eventId()]) {
+               return true; 
+            }
+         } else if (tagName == "Eco") {
+            if (vb[m_gameIndex[gameId].ecoId()]) {
+               return true; 
+            }
+         }
+         return false;
+      case Search::EloSearch:
+         es = static_cast<EloSearch*> (search);
+         return es->withinEloRange(m_gameIndex[gameId].whiteElo(), m_gameIndex[gameId].blackElo());
+      case Search::DateSearch:
+         ds = static_cast<DateSearch*> (search);
+         return ds->withinDateRange(m_gameIndex[gameId].date());
+      default :
+         // Unhandled search
+         break;
+   }
+
+   return false;
+
+}
+
+Q_UINT32 Index::whiteId(Q_UINT32 index)
+{
+   return m_gameIndex[index].whiteId();
+}
+QString Index::white(Q_UINT32 index,Tags& tags)
+{
+   return tags.value(Tags::PlayerName,m_gameIndex[index].whiteId());
+}
+Q_UINT32 Index::blackId(Q_UINT32 index)
+{
+   return m_gameIndex[index].blackId();
+}
+QString Index::black(Q_UINT32 index, Tags& tags)
+{
+   return tags.value(Tags::PlayerName,m_gameIndex[index].blackId());
+}
+Q_UINT32 Index::eventId(Q_UINT32 index)
+{
+   return m_gameIndex[index].eventId();
+}
+QString Index::event(Q_UINT32 index, Tags& tags)
+{
+   return tags.value(Tags::Event,m_gameIndex[index].eventId());
+}
+Q_UINT32 Index::siteId(Q_UINT32 index)
+{
+   return m_gameIndex[index].siteId();
+}
+QString Index::site(Q_UINT32 index, Tags& tags)
+{
+   return tags.value(Tags::Site,m_gameIndex[index].siteId());
+}
+Q_UINT16 Index::whiteElo(Q_UINT32 index)
+{
+   return m_gameIndex[index].whiteElo();
+}
+Q_UINT16 Index::blackElo(Q_UINT32 index)
+{
+   return m_gameIndex[index].blackElo();
+}
+Q_UINT16 Index::ecoId(Q_UINT32 index)
+{
+   return m_gameIndex[index].ecoId();
+}
+QString Index::eco(Q_UINT32 index, Tags& tags)
+{
+   return tags.value(Tags::ECO, m_gameIndex[index].ecoId());
+}
+PartialDate Index::date(Q_UINT32 index)
+{
+   return m_gameIndex[index].date();
+}
+QString Index::dateString(Q_UINT32 index)
+{
+   return m_gameIndex[index].date().asString();
+}
+
+Result Index::result(Q_UINT32 index)
+{
+   return m_gameIndex[index].result();
+}
+Q_UINT8 Index::round(Q_UINT32 index)
+{
+   return m_gameIndex[index].round();
+}
---
+++ trunk/src/database/indexitem.h
@@ -0,0 +1,102 @@
+/***************************************************************************
+                          indexitem.h - description
+                             -------------------
+    begin                : 
+    copyright            : (C) 2006 Marius Roets <saidinwielder@sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __INDEXITEM_H__
+#define __INDEXITEM_H__
+
+#include <qglobal.h>
+#include "partialdate.h"
+#include "qt34/qvaluelist.h"
+#include "common.h"
+
+/** The IndexItem class holds one item that is listed in a index
+ * It contains essential game header information that is kept in
+ * memory for fast access. It is used in conjunction with the Tags
+ * class. For most items it stores the id that refers to the relevant
+ * tag item in the Tags instance. Where convenient, the actual value is
+ * stored instead of the id.
+ */
+
+class IndexItem {
+   public:
+      IndexItem();
+      /** Sets the tag id for the black player name */
+      void setBlackId(Q_UINT32 id);
+      /** Sets the tag id for the white player name */
+      void setWhiteId(Q_UINT32 id);
+      /** Sets the tag id for the event name */
+      void setEventId(Q_UINT32 id);
+      /** Sets the tag id for the site name */
+      void setSiteId(Q_UINT32 id);
+      /** Sets the Elo rating of the white player */
+      void setWhiteElo(Q_UINT16 elo);
+      /** Sets the Elo rating of the black player */
+      void setBlackElo(Q_UINT16 elo);
+      /** Sets the tag id for the ECO code of the game */
+      void setEcoId(Q_UINT16 id);
+      /** Sets the result of the game */
+      void setResult(Result result);
+      /** Sets the round of the game */
+      void setRound(Q_UINT8 round);
+      /** Sets the date of the game */
+      void setDate(const PartialDate& date);
+      /** Returns the tag id for the black player name */
+      Q_UINT32 blackId();
+      /** Returns the tag id for the white player name */
+      Q_UINT32 whiteId();
+      /** Returns the tag id for the event name */
+      Q_UINT32 eventId();
+      /** Returns the tag id for the site name */
+      Q_UINT32 siteId();
+      /** Returns the Elo rating of the white player */
+      Q_UINT16 whiteElo();
+      /** Returns the Elo rating of the black player */
+      Q_UINT16 blackElo();
+      /** Returns tag id for the ECO code of the game */
+      Q_UINT16 ecoId();
+      /** Returns the round of the game */
+      Q_UINT8 round();
+      /** Returns the result of the game */
+      Result result();
+      /** Returns the date of the game */
+      PartialDate date();
+
+      /** Static method to test whether tag name is supported in the index */
+      static bool isTagNameSupported( const QString& tagName);
+      /** Static method to populate list of supported tags */
+      static void setSupportedTagName();
+
+
+   private:
+
+      Q_UINT32 m_whiteId; // 4 bytes
+      Q_UINT32 m_blackId; // 4 bytes
+      Q_UINT32 m_eventId; // 4 bytes
+      Q_UINT32 m_siteId; // 4 bytes
+      Q_UINT16 m_whiteElo; // 2 bytes
+      Q_UINT16 m_blackElo; // 2 bytes
+      Q_UINT16 m_ecoId; // 2 bytes
+      Result m_result; // 1 bytes
+      Q_UINT8 m_round; // 1 bytes
+      PartialDate m_date; // 4 bytes? (could use a date Id?)
+
+      // Total 28 bytes / 
+      static QValueList<QString> m_supportedTagNames;
+
+};
+
+#endif	// __INDEXITEM_H__
+
