4a820adf7e3e94137d88e66444b292f4dc48a800
hhex <hhex@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-01-23 06:28:48 +0000

    Unit tests using qtestlib

---
+++ tests/qtestlib/pgndatabasetest.cpp
@@ -0,0 +1,128 @@
+/***************************************************************************
+                          common  -  description
+                             -------------------
+    begin                : 30/04/2006
+    copyright            : (C) 2006 Heinz R. Hopfgartner 
+                           <heinz.hopfgartner@gmx.at>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+Unit tests for the PgnDatabase class.
+*/
+
+#include <qdir.h>
+
+#include "pgndatabasetest.h"
+#include "pgndatabase.h"
+#include "game.h"
+#include "filter.h"
+#include "search.h"
+
+
+
+void PgnDatabaseTest::initTestCase() {}
+void PgnDatabaseTest::init(){}
+void PgnDatabaseTest::cleanup(){}
+
+void PgnDatabaseTest::cleanupTestCase() {
+    QDir toRemove;
+    toRemove.remove( "./data/new.pgn" );
+    toRemove.remove( "./data/new1.pgn" );
+}
+
+void PgnDatabaseTest::testCreateDatabase() {
+  PgnDatabase* db = new PgnDatabase();
+  db->create(  QString( "./data/game1.pgn" ) );
+  QCOMPARE( db->count(), 2 );
+  delete db;
+}
+
+void PgnDatabaseTest::testName() {
+    PgnDatabase* db = new PgnDatabase();
+    db->open(  QString( "./data/game1.pgn" ) );
+    const QString name = QString("./data/game1.pgn");
+    QCOMPARE( db->filename(), name );
+    delete db;
+
+}
+
+void PgnDatabaseTest::testLoad() {
+    PgnDatabase* db = new PgnDatabase();
+    db->open(  QString( "./data/game1.pgn" ) );
+    Game game;
+    QCOMPARE( 2 , db->count() );
+    QCOMPARE( db->load( 1, game ), true );
+    QCOMPARE( db->load( 0, game ), true );
+    QCOMPARE( db->load( 2, game ), false );
+    delete db;
+
+}
+
+void PgnDatabaseTest::testCopyGameIntoNewDB() {
+    PgnDatabase* db = new PgnDatabase();
+    db->open(  QString( "./data/game1.pgn" ) );
+    Game game;
+    bool success = db->load( 1, game );
+    PgnDatabase* dbNew = new PgnDatabase();
+    dbNew->create( QString("./data/new.pgn" ));
+    dbNew->add( game );
+    success = db->load( 0, game );
+    dbNew->add( game );
+    QCOMPARE( dbNew->count(), 2 );
+    delete dbNew;
+    delete db;
+}
+
+void PgnDatabaseTest::testRemoveGame() {
+    PgnDatabase* dbNew = new PgnDatabase();
+    dbNew->open( QString("./data/new.pgn" ));
+    dbNew->remove( 1 );
+    QCOMPARE( dbNew->count(), 1 );
+    dbNew->remove( 0 );
+    QCOMPARE( dbNew->count(), 0 );
+    delete dbNew;
+}
+
+// void PgnDatabaseTest::testExecuteSearch() {
+//     PgnDatabase* db = new PgnDatabase();
+//     db->open( QString( "./data/game1.pgn" ));
+//     TagSearch search;
+//     search.setTag( "Black" );
+//     search.setValue( "Capablanca, Jose Raul" );
+//     Filter filter = db->executeSearch( search );
+//     QCOMPARE( 2, filter.count() );
+
+//     TagSearch search1;
+//     search1.setTag( "Result" );
+//     search1.setValue( "1-0" );
+//     Filter filter1 = db->executeSearch( search1 );
+//     QCOMPARE( 1, filter1.count() );
+//     delete db;
+// }
+
+void PgnDatabaseTest::testSave() {
+    PgnDatabase* db = new PgnDatabase();
+    db->open( QString( "./data/game1.pgn" ));
+    Game game;
+    QCOMPARE( true, db->load( 1, game ) );
+    
+    PgnDatabase* dbNew = new PgnDatabase();
+    dbNew->create( QString( "./data/new1.pgn" ));
+    
+    QCOMPARE( dbNew->save( 0, game ), true );
+    QCOMPARE( db->load( 0, game ), true );
+    QCOMPARE( true, dbNew->save( 1, game ) );
+    QCOMPARE( dbNew->count(), 2 );
+    delete dbNew;
+    delete db;
+}
+
+
---
+++ tests/qtestlib/spellcheckertests.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+                          spellcheckertests.h  -  description
+                             -------------------
+    begin                : 15/09/2005
+    copyright            : (C) 2005, 2006 Heinz R. Hopfgartner
+                           <heinz.hopfgartner@gmx.at>
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+   Test the speellchecker class.
+*/
+
+
+#ifndef __SPELLCHECKERTESTS_H 
+#define __SPELLCHECKERTESTS_H  
+
+#include<spellchecker.h> 
+#include <QtTest/QtTest>
+
+
+class SpellCheckerTests : public QObject
+{
+    
+Q_OBJECT
+
+private slots:
+
+  void initTestCase();
+  void init();
+  void cleanup();
+  void cleanupTestCase();
+
+  void testNoRule();
+  void testImportSpellingFile();
+  void testSaveSpellings();
+  void testRemoveRules();
+  void testLoadSpellings();
+  void testCorrectPlayer();
+  void testCorrectSite();
+  void testCorrectEvent();
+  void testCorrectRound();
+  void testFindSpellingsLiteral();
+  void testAddRuleSpelling();
+  void testRemoveRule();
+  void testRemoveRuleFail();
+  void testFindSpellingsLiteralInNewSpell();
+
+ private:
+
+  /** used to import the scid spellchecker file */
+  Spellchecker speller;
+
+  /** used for a blank spellchecker file */
+  Spellchecker newSpell;
+}; 
+
+
+#endif 
