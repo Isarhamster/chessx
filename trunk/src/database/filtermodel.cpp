e51fca8d42e01029a064fd23d0b75eeb6a31bcd9
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-02-13 10:58:37 +0000

    Rename FilterModelBase class to DatabaseModel to reflect current behavior.

--- trunk/src/database/filtermodel.cpp
+++ trunk/src/database/filtermodel.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-                          FilterModelBase.cpp - Provides a model for the Filter class
+                          DatabaseModel.cpp - Provides a model for the Filter class
                                             in term of the Qt filter/model framework
                              -------------------
     begin                : 17/01/2007
@@ -17,8 +17,8 @@
 
 #include "filtermodel.h"
 
-FilterModelBase::FilterModelBase(Filter*filter, QObject* parent)
-         : QAbstractItemModel(parent), m_filter(filter)
+DatabaseModel::DatabaseModel(Database* database, QObject* parent)
+         : QAbstractItemModel(parent), m_database(database)
 {
    m_columnNames << "White"
                  << "Black"
@@ -31,43 +31,44 @@ FilterModelBase::FilterModelBase(Filter*filter, QObject* parent)
                  << "Length";
    m_game = new Game;
 }
-FilterModelBase::~FilterModelBase()
+DatabaseModel::~DatabaseModel()
 {
    delete m_game;
 }
 
-int FilterModelBase::rowCount(const QModelIndex& index) const
+int DatabaseModel::rowCount(const QModelIndex& index) const
 {
   if (index.isValid())
     return 0;
-  return m_filter->count();
+  return m_database->count();
 }
 
-int FilterModelBase::columnCount(const QModelIndex&) const
+int DatabaseModel::columnCount(const QModelIndex&) const
 {
    return m_columnNames.count();
 }
 
-QVariant FilterModelBase::data(const QModelIndex &index, int role) const
+QVariant DatabaseModel::data(const QModelIndex &index, int role) const
 {
    if (!index.isValid())
       return QVariant();
-   if (index.row() >= m_filter->count()) {
+   if (index.row() >= m_database->count())
       return QVariant();
-   }
 
    if (role == Qt::DisplayRole) 
    {
-     m_filter->database()->loadHeaders(index.row(), *m_game);
-     if (index.column() == 0)
+     m_database->loadHeaders(index.row(), *m_game);
+     /* 
+      if (index.column() == 0)
        qDebug("Loading game %d: %s-%s", index.row(),
              qPrintable(m_game->tag(m_columnNames.at(0))), qPrintable(m_game->tag(m_columnNames.at(1))));
+     */
      return m_game->tag(m_columnNames.at(index.column()));
    }
    else return QVariant();
 }
 
-QVariant FilterModelBase::headerData(int section, Qt::Orientation orientation, int role) const
+QVariant DatabaseModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
    if (role != Qt::DisplayRole)
       return QVariant();
@@ -78,25 +79,27 @@ QVariant FilterModelBase::headerData(int section, Qt::Orientation orientation, i
       return QString("%1").arg(section);
 }
 
-QModelIndex FilterModelBase::index(int row, int column, const QModelIndex& parent) const
+QModelIndex DatabaseModel::index(int row, int column, const QModelIndex& parent) const
 {
   if (parent.isValid())
     return QModelIndex();
   return createIndex(row, column);
 }
 
-void FilterModelBase::setFilter(Filter* filter)
+void DatabaseModel::setDatabase(Database* database)
 {
-  m_filter = filter;
+  m_database = database;
   reset();
 }
+
+
 
 
 
 FilterModel::FilterModel(Filter* filter, QObject* parent) : QSortFilterProxyModel(parent)
 {
   m_filter = filter;
-  FilterModelBase* source = new FilterModelBase(filter, parent);
+  DatabaseModel* source = new DatabaseModel(filter->database(), parent);
   setSourceModel(source);
 }
 
@@ -104,17 +107,22 @@ FilterModel::FilterModel(Filter* filter, QObject* parent) : QSortFilterProxyMode
 void FilterModel::setFilter(Filter* filter)
 {
   m_filter = filter;
-  qobject_cast<FilterModelBase*>(sourceModel())->setFilter(filter);
+  qobject_cast<DatabaseModel*>(sourceModel())->setDatabase(filter->database());
 }
 
-bool FilterModel::filterAcceptsRow(int source_row, const QModelIndex& index) const
+bool FilterModel::filterAcceptsRow(int source_row, const QModelIndex&) const
 {
-  qDebug("%d in filter? %s", source_row, m_filter->contains(source_row) ? "Yes" : "No");
+  //qDebug("%s%d ", m_filter->contains(source_row) ? "+" : "-", source_row);
   return m_filter->contains(source_row);
 }
 
 FilterModel::~FilterModel()
 {
   delete sourceModel();
+}
+
+void FilterModel::updateFilter()
+{
+  filterChanged();
 }
 
