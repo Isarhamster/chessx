8d563629b6b35cf5002eacbfe5c7b2fd4c5f5e06
saidinwielder <saidinwielder@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-03-01 07:44:54 +0000

    Fix by Kamil Rytarowski to enable finding data path on Debian.

--- src/database/settings.cpp
+++ src/database/settings.cpp
@@ -67,25 +67,30 @@ void Settings::setLayout(const QWidget* w)
 
 QString Settings::dataPath()
 {
-  if (m_dataPath.isNull())
-  {
-    m_dataPath = qApp->applicationDirPath();
-    if (m_dataPath.endsWith("/games/bin"))
-    {
-      m_dataPath.truncate(m_dataPath.length() - 10);
-      m_dataPath.append("/share/games/chessx");
-    }
-    else
-    {
-     if(m_dataPath.endsWith("/chessdatabase.app/Contents/MacOS"))
-	 //Changed from 33 to 15 to include the data directory in the application directory for MaxOSX
-       m_dataPath.truncate(m_dataPath.length() - 15);
-     if (m_dataPath.endsWith("/bin"))
-       m_dataPath.truncate(m_dataPath.length() - 4);
-     m_dataPath.append("/data");
-    }
-  }
-  return m_dataPath;
+   if (m_dataPath.isNull())
+   {
+      m_dataPath = qApp->applicationDirPath();
+      if (m_dataPath.endsWith("/games/bin"))
+      {
+         m_dataPath.truncate(m_dataPath.length() - 10);
+         m_dataPath.append("/share/games/chessx");
+      }
+      else if (m_dataPath.endsWith("/games"))
+      {
+         m_dataPath.truncate(m_dataPath.length() - 6);
+         m_dataPath.append("/share/games/chessx");
+      }
+      else
+      {
+         if(m_dataPath.endsWith("/chessdatabase.app/Contents/MacOS"))
+            //Changed from 33 to 15 to include the data directory in the application directory for MaxOSX
+            m_dataPath.truncate(m_dataPath.length() - 15);
+         if (m_dataPath.endsWith("/bin"))
+            m_dataPath.truncate(m_dataPath.length() - 4);
+         m_dataPath.append("/data");
+      }
+   }
+   return m_dataPath;
 }
 
 QString Settings::iconPath(const QString& name)
