aa9fd844af91d2fe878b298be465e883cdc98ea9
saidinwielder <saidinwielder@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-03-27 19:01:05 +0000

    Added MemoryDatabase

---
+++ src/database/memorydatabase.cpp
@@ -0,0 +1,97 @@
+/***************************************************************************
+                          filename - description
+                             -------------------
+    begin                : 21/03/2007
+    copyright            : (C) 2007 Marius Roets <saidinwielder@sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QtDebug>
+#include "memorydatabase.h"
+
+MemoryDatabase::MemoryDatabase()
+{
+}
+MemoryDatabase::~MemoryDatabase()
+{
+   for (int i = 0; i < m_games.count(); ++i) {
+      delete m_games[i];
+   }
+}
+bool MemoryDatabase::add(Game& game)
+{
+   int index = m_count;
+   // Add to index
+   QMap <QString, QString> tags = game.tags();
+   QMap <QString, QString>::const_iterator i = tags.constBegin();
+   while (i != tags.constEnd()) {
+      m_index.setTag(i.key(),i.value(),index);
+      ++i;
+   }
+   // Upate game array
+   Game* newGame = new Game;
+   *newGame = game;
+   newGame->clearTags();
+   m_games.append(newGame);
+   m_count++;
+   return true;
+}
+bool MemoryDatabase::remove(int )
+{
+   return false;
+}
+bool MemoryDatabase::remove(const Filter& )
+{
+   return false;
+}
+bool MemoryDatabase::replace(int index, Game& game)
+{
+   if (index >= m_count) {
+      return false;
+   }
+   // Update index
+   QMap <QString, QString> tags = game.tags();
+   QMap <QString, QString>::const_iterator i = tags.constBegin();
+   while (i != tags.constEnd()) {
+      m_index.setTag(i.key(),i.value(),index);
+      ++i;
+   }
+   // Upate game array
+   *m_games[index] = game;
+   m_games[index]->clearTags();
+   return true;
+}
+bool MemoryDatabase::loadGame(int index, Game& game)
+{
+	if(!m_isOpen || index >= m_count) {
+		return false;
+	}
+	game.clear();
+   game = *m_games[index];
+   loadGameHeaders(index, game);
+
+   return true;
+}
+bool MemoryDatabase::parseFile()
+{
+   m_index.setCacheEnabled(true);
+	//indexing game positions in the file, game contents are ignored
+	while(!m_file->atEnd()) {		
+		readJunk();
+		addOffset();
+		parseTagsIntoIndex(); // This will parse the tags into memory
+      Game* game = new Game;
+		parseMoves(game);
+      m_games.append(game);
+	}
+   m_index.setCacheEnabled(false);
+   return true;
+}
---
+++ src/database/memorydatabase.h
@@ -0,0 +1,57 @@
+/***************************************************************************
+                          filename - description
+                             -------------------
+    begin                : 21/03/2007
+    copyright            : (C) 2007 Marius Roets <saidinwielder@sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __MEMORYDATABASE_H__
+#define __MEMORYDATABASE_H__
+
+#include <QVector>
+#include "pgndatabase.h"
+
+/** @ingroup Database
+   The MemoryDatabase class provides database access to PGN files.
+   Games are stored in memory, and are editable.
+   The class is derived from the PgnDatabase class, providing methods for the 
+   loading and saving of games, and for performing searches and queries.
+*/
+
+class MemoryDatabase : public PgnDatabase
+{
+   public:
+		/** Default constructor */
+		MemoryDatabase();
+		/** Destructor */
+		~MemoryDatabase();
+      /** Returns whether the database is read-only or not */
+      bool isReadOnly() { return false; }
+      /** Adds a game to the database */
+      bool add(Game& game);
+      /** Removes a game from the database */
+      bool remove(int index);
+      /** Removes multiple games from the database as specified by the filter */
+      bool remove(const Filter& filter);
+      /** Saves a game at the given position, returns true if successful */
+      bool replace(int index, Game& game);
+
+		/** Loads a game from the given position, returns true if successful */
+		bool loadGame(int index, Game& game);
+
+   private:
+      QVector <Game*> m_games;
+      bool parseFile();
+};
+
+#endif	// __MEMORYDATABASE_H__
+
