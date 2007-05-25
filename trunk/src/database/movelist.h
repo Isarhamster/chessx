9956995e1a07cc047be9acf70b8f85685ed753c0
tux68 <tux68@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-05-25 23:40:51 +0000

    Custom MoveList class.
    Performance traces show that the  QVector version of this class has a
    bit too much overhead.

--- trunk/src/database/movelist.h
+++ trunk/src/database/movelist.h
@@ -15,8 +15,27 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <QVector>
+/** @ingroup Core
+   Low level list of moves, used by bitboard
+*/
 
+#ifndef __MOVELIST_H__
+#define __MOVELIST_H__
 #include "move.h"
 
-typedef QVector<Move> MoveList;
+class MoveList
+{
+public:
+	MoveList() : m_cnt(0) {}
+	// There really really must be a better way.
+	Move& add() { Q_ASSERT(m_cnt < 90); return m_moves[m_cnt++]; };
+	int size() { return m_cnt; };
+	bool empty() { return !m_cnt; };
+	void append(const Move& move) { Q_ASSERT(m_cnt < 90); m_moves[m_cnt++] = move; };
+	Move& operator[](int index) {return m_moves[index];};
+private:
+	Move m_moves[500];		// Later This should be made dynamic.
+	int m_cnt;
+};
+
+#endif // __MOVELIST_H__
