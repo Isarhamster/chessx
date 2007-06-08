7b32c77147a7c5868eafd6f0f106b83667e59b3e
tux68 <tux68@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-06-08 04:27:54 +0000

    Mark some MoveList class methods as const.

--- trunk/src/database/movelist.h
+++ trunk/src/database/movelist.h
@@ -29,10 +29,10 @@ public:
 	MoveList() : m_cnt(0) {}
 	// There really really must be a better way.
 	Move& add() { Q_ASSERT(m_cnt < 90); return m_moves[m_cnt++]; };
-	int size() { return m_cnt; };
+	int size() const { return m_cnt; };
 	bool empty() { return !m_cnt; };
 	void append(const Move& move) { Q_ASSERT(m_cnt < 90); m_moves[m_cnt++] = move; };
-	Move& operator[](int index) {return m_moves[index];};
+	const Move& operator[](int index) const {return m_moves[index];};
 private:
 	Move m_moves[500];		// Later This should be made dynamic.
 	int m_cnt;
