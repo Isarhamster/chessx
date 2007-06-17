b9e89f7398c18bb56d0d4795ac509828237fb598
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-06-17 22:21:11 +0000

    New TableView widget.

---
+++ trunk/src/gui/tableview.h
@@ -0,0 +1,34 @@
+/* QTableView with some adjustments like storing header layout */
+/* (C) 2007 Michal Rudolf <mrudolf@kdewebdev.org> */
+
+#ifndef TABLEVIEW_H
+#define TABLEVIEW_H
+
+#include <QTableView>
+
+/**
+	The TableView class is a specialized version of QTableView with automatic
+	header layout saving.
+
+*/
+
+class QAction;
+
+class TableView : public QTableView
+{
+Q_OBJECT
+public:
+   TableView(QWidget *parent = 0);
+	~TableView();
+public slots:
+	/** Store current configuration. */
+	void saveConfig();
+	/** Restore current configuration. */
+	void slotReconfigure();
+	/** Toggle font */
+	void slotToggleFont(bool toggled);
+private:
+	QAction* m_smallfont;
+};
+
+#endif
