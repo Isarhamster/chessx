b22e9b2566e2ce3cfb8be3841d65a0fcd443d6dd
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2007-11-08 16:01:43 +0000

    Allow editing of annotations. It seems that due to Game bug it doesn't find proper annotation - this should be fixed soon.

--- trunk/src/gui/mainwindow.cpp
+++ trunk/src/gui/mainwindow.cpp
@@ -844,6 +844,7 @@ void MainWindow::slotGameSave()
 void MainWindow::slotGameModify(int action, int move)
 {
 	game().moveToId(move);
+	slotMoveChanged();
 	switch (action) {
 		case ChessBrowser::RemoveNextMoves:
 			game().truncateGameEnd();
@@ -856,6 +857,15 @@ void MainWindow::slotGameModify(int action, int move)
 				int var = game().currentVariation();
 				game().exitVariation();
 				game().removeVariation(var);
+				break;
+			}
+		case ChessBrowser::EditComment:
+			{
+				bool ok;
+				QString cmt = QInputDialog::getText(this, tr("Edit comment"), tr("Comment:"),
+						QLineEdit::Normal, game().annotation(game().currentVariation()), &ok);
+				if (ok)
+					game().setAnnotation(cmt, (game().currentVariation()));
 				break;
 			}
 			default:
