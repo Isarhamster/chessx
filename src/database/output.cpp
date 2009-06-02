022cec94a8209d4b48027197e9b1530ccc19edaf
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2009-06-02 12:17:53 +0000

    Fix: don't write empty EventDate (patch by Alessandro Gandellli)

--- src/database/output.cpp
+++ src/database/output.cpp
@@ -342,7 +342,7 @@ void Output::writeAllTags()
 	while (it.hasNext()) {
 		it.next();
 		// workaround for problems with IndexItem implementation
-		if (!it.value().isEmpty() && it.value() != "?" && it.key() != "Length")
+		if (!it.value().isEmpty() && it.value() != "?" && it.value() != "????.??.??" && it.key() != "Length")
 			writeTag(it.key(), it.value());
 	}
 }
