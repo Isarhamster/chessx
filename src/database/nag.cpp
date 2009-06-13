8612c08bd5bc6d83d6a7b6946a7e741b821448a7
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2009-06-13 11:36:22 +0000

    Cleanup: simplify Nag class (patch by Nikita Manovich).

--- src/database/nag.cpp
+++ src/database/nag.cpp
@@ -17,106 +17,27 @@
 
 #include "nag.h"
 
-NagSet::NagSet()
-{
-	m_noNags = 0;
-}
-
-NagSet::NagSet(Nag nag)
-{
-	if (nag == NullNag) {
-		m_noNags = 0;
-	} else {
-		m_nags[0] = nag;
-		m_noNags = 1;
-	}
-}
-
-NagSet::NagSet(const QVector<Nag>& nags)
-{
-	m_noNags = MaxNags < (int)nags.count() ? MaxNags : nags.count();
-	for (int nag = 0; nag <= m_noNags; nag++) {
-		m_nags[nag] = nags[nag];
-	}
-}
-NagSet& NagSet::operator=(const NagSet& nagset)
-{
-	m_noNags = MaxNags < (int)nagset.count() ? MaxNags : nagset.count();
-	for (int nag = 0; nag <= m_noNags; nag++) {
-		m_nags[nag] = nagset[nag];
-	}
-
-	return *this;
-}
-Nag NagSet::operator[](int index)
-{
-	return (Nag)m_nags[index];
-}
-Nag NagSet::operator[](int index) const
-{
-	return (Nag)m_nags[index];
-}
-
 void NagSet::addNag(Nag nag)
 {
-	if (nag != NullNag && m_noNags < MaxNags) {
-		m_nags[m_noNags++] = nag;
-	}
+	if (nag != NullNag) push_back(nag);
 }
 
 void NagSet::removeNag(Nag nag)
 {
-	if (nag == NullNag) {
-		return;
-	}
+	if (nag == NullNag) return;
 
-	int index;
-	bool found = false;
-
-	for (index = 0; index < m_noNags; index++) {
-		if (m_nags[index] == nag) {
-			found = true;
-			break;
-		}
-	}
-
-	if (found) {
-		for (index++; index < m_noNags; index++) {
-			m_nags[index - 1] = m_nags[index];
-		}
-		m_noNags--;
-	}
+	int index = indexOf(nag);
+	if (index != -1) remove(index);
 }
 
-void NagSet::clear()
+QString NagSet::toString(unsigned format) const
 {
-	m_noNags = 0;
-}
+	QString moveNag;
+	QString evaluationNag;
+	QString otherNags;
 
-int NagSet::count() const
-{
-	return m_noNags;
-}
-
-QVector<Nag> NagSet::nags() const
-{
-	QVector<Nag> nags;
-
-	for (int nag = 0; nag < m_noNags; nag++) {
-		nags.push_back((Nag)m_nags[nag]);
-	}
-
-	return nags;
-}
-
-QString NagSet::toString() const
-{
-	QString moveNag = "";
-	QString evaluationNag = "";
-	QString otherNags = "";
-
-	for (int nag = 0; nag < m_noNags; nag++) {
-		switch (m_nags[nag]) {
+	for (unsigned i = 0; i < count(); i++) {
+		switch (this->at(i)) {
 		case GoodMove:
 		case PoorMove:
 		case VeryGoodMove:
@@ -126,7 +47,7 @@ QString NagSet::toString() const
 		case ForcedMove:
 		case SingularMove:
 		case WorstMove:
-			moveNag = nagToString((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
+			moveNag = format == PGN ? "$" + QString::number(at(i)) : nagToString(at(i));
 			break;
 		case DrawishPosition:
 		case EqualChancesQuietPosition:
@@ -140,87 +61,178 @@ QString NagSet::toString() const
 		case BlackHasADecisiveAdvantage:
 		case WhiteHasACrushingAdvantage:
 		case BlackHasACrushingAdvantage:
-			evaluationNag = nagToString((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
+			evaluationNag = format == PGN ? "$" + QString::number(at(i)) : nagToString(at(i));
 			break;
+
 		default:
-			otherNags = nagToString((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
+			otherNags = format == PGN ? "$" + QString::number(at(i)) : nagToString(at(i));
 			break;
 		}
 	}
 
-	otherNags = otherNags.simplified();
-
-	return moveNag + evaluationNag + otherNags;
+	return moveNag + evaluationNag + otherNags.simplified();
 }
 
-QString NagSet::toPGNString() const
+static const QString g_nagStringList[NagCount] = {
+	"",
+	"!",
+	"?",
+	"!!",
+	"??",
+	"!?",
+	"?!",
+	"forced move",
+	"singular move",
+	"??",
+	"=",
+	"=, quiet position",
+	"=, active position",
+	"~",
+	"+=",
+	"=+",
+	"+/-",
+	"-/+",
+	"+-",
+	"-+",
+	"+-",
+	"-+",
+	"White is in zugzwang",
+	"Black is in zugzwang",
+	"White has a slight space advantage",
+	"Black has a slight space advantage",
+	"White has a moderate space advantage",
+	"Black has a moderate space advantage",
+	"White has a decisive space advantage",
+	"Black has a decisive space advantage",
+	"White has a slight time (development) advantage",
+	"Black has a slight time (development) advantage",
+	"White has a moderate time (development) advantage",
+	"Black has a moderate time (development) advantage",
+	"White has a decisive time (development) advantage",
+	"Black has a decisive time (development) advantage",
+	"White has the initiative",
+	"Black has the initiative",
+	"White has a lasting initiative",
+	"Black has a lasting initiative",
+	"White has the attack",
+	"Black has the attack",
+	"White has insufficient compensation for material deficit",
+	"Black has insufficient compensation for material deficit",
+	"~=",
+	"~=",
+	"White has more than adequate compensation for material deficit",
+	"Black has more than adequate compensation for material deficit",
+	"White has a slight center control advantage",
+	"Black has a slight center control advantage",
+	"White has a moderate center control advantage",
+	"Black has a moderate center control advantage",
+	"White has a decisive center control advantage",
+	"Black has a decisive center control advantage",
+	"White has a slight kingside control advantage",
+	"Black has a slight kingside control advantage",
+	"White has a moderate kingside control advantage",
+	"Black has a moderate kingside control advantage",
+	"White has a decisive kingside control advantage",
+	"Black has a decisive kingside control advantage",
+	"White has a slight queenside control advantage",
+	"Black has a slight queenside control advantage",
+	"White has a moderate queenside control advantage",
+	"Black has a moderate queenside control advantage",
+	"White has a decisive queenside control advantage",
+	"Black has a decisive queenside control advantage",
+	"White has a vulnerable first rank",
+	"Black has a vulnerable first rank",
+	"White has a well protected first rank",
+	"Black has a well protected first rank",
+	"White has a poorly protected king",
+	"Black has a poorly protected king",
+	"White has a well protected king",
+	"Black has a well protected king",
+	"White has a poorly placed king",
+	"Black has a poorly placed king",
+	"White has a well placed king",
+	"Black has a well placed king",
+	"White has a very weak pawn structure",
+	"Black has a very weak pawn structure",
+	"White has a moderately weak pawn structure",
+	"Black has a moderately weak pawn structure",
+	"White has a moderately strong pawn structure",
+	"Black has a moderately strong pawn structure",
+	"White has a very strong pawn structure",
+	"Black has a very strong pawn structure",
+	"White has poor knight placement",
+	"Black has poor knight placement",
+	"White has good knight placement",
+	"Black has good knight placement",
+	"White has poor bishop placement",
+	"Black has poor bishop placement",
+	"White has good bishop placement",
+	"Black has good bishop placement",
+	"White has poor rook placement",
+	"Black has poor rook placement",
+	"White has good rook placement",
+	"Black has good rook placement",
+	"White has poor queen placement",
+	"Black has poor queen placement",
+	"White has good queen placement",
+	"Black has good queen placement",
+	"White has poor piece coordination",
+	"Black has poor piece coordination",
+	"White has good piece coordination",
+	"Black has good piece coordination",
+	"White has played the opening very poorly",
+	"Black has played the opening very poorly",
+	"White has played the opening poorly",
+	"Black has played the opening poorly",
+	"White has played the opening well",
+	"Black has played the opening well",
+	"White has played the opening very well",
+	"Black has played the opening very well",
+	"White has played the middlegame very poorly",
+	"Black has played the middlegame very poorly",
+	"White has played the middlegame poorly",
+	"Black has played the middlegame poorly",
+	"White has played the middlegame well",
+	"Black has played the middlegame well",
+	"White has played the middlegame very well",
+	"Black has played the middlegame very well",
+	"White has played the ending very poorly",
+	"Black has played the ending very poorly",
+	"White has played the ending poorly",
+	"Black has played the ending poorly",
+	"White has played the ending well",
+	"Black has played the ending well",
+	"White has played the ending very well",
+	"Black has played the ending very well",
+	"White has slight counterplay",
+	"Black has slight counterplay",
+	"White has moderate counterplay",
+	"Black has moderate counterplay",
+	"White has decisive counterplay",
+	"Black has decisive counterplay",
+	"White has moderate time control pressure",
+	"Black has moderate time control pressure",
+	"White has severe time control pressure",
+	"Black has severe time control pressure",
+	"with the idea",
+	"aimed against",
+	"Better was",
+	"Worse was",
+	"Equivalent was",
+	"RR",
+	"N",
+	"weak point",
+	"endgame",
+	"line",
+	"diagonal",
+	"White has a pair of bishops",
+	"Black has a pair of bishops",
+	"Bishops of opposite color",
+	"Bishops of same color"
+};
+
+const QString& NagSet::nagToString(Nag nag)
 {
-	QString moveNag = "";
-	QString evaluationNag = "";
-	QString otherNags = "";
-
-	for (int nag = 0; nag < m_noNags; nag++) {
-		switch (m_nags[nag]) {
-		case GoodMove:
-		case PoorMove:
-		case VeryGoodMove:
-		case VeryPoorMove:
-		case SpeculativeMove:
-		case QuestionableMove:
-		case ForcedMove:
-		case SingularMove:
-		case WorstMove:
-			moveNag = "$" + QString::number((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
-			break;
-		case DrawishPosition:
-		case EqualChancesQuietPosition:
-		case EqualChancesActivePosition:
-		case UnclearPosition:
-		case WhiteHasASlightAdvantage:
-		case BlackHasASlightAdvantage:
-		case WhiteHasAModerateAdvantage:
-		case BlackHasAModerateAdvantage:
-		case WhiteHasADecisiveAdvantage:
-		case BlackHasADecisiveAdvantage:
-		case WhiteHasACrushingAdvantage:
-		case BlackHasACrushingAdvantage:
-			evaluationNag = "$" + QString::number((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
-			break;
-		default:
-			otherNags += "$" + QString::number((Nag)m_nags[nag]);// + " "; Removed to avoid double spaces after nags.
-			break;
-		}
-	}
-
-	otherNags = otherNags.simplified();
-
-	return moveNag + evaluationNag + otherNags;
+	return g_nagStringList[nag];
 }
 
-QString NagSet::nagToString(Nag nag)
-{
-	return NagStrings[nag];
-}
-
-Nag NagSet::stringToNag(const QString& nagString)
-{
-	//initialize string map: if required
-	if (!initialized) {
-		initialize();
-	}
-
-	return stringMap[nagString];
-}
-
-void NagSet::initialize()
-{
-	for (int nag = 0; nag < NagCount; nag++) {
-		stringMap[NagStrings[nag]] = (Nag)nag;
-	}
-
-	initialized = true;
-}
-
-//static data members
-bool NagSet::initialized = false;
-QMap<QString, Nag> NagSet::stringMap;
--- src/database/nag.h
+++ src/database/nag.h
@@ -19,10 +19,11 @@
 #define __NAG_H__
 
 #include <QString>
-#include <QMap>
 #include <QVector>
 
-typedef enum {
+/* Don't forget to add string for each 'nag' in source file. */
+typedef enum
+{
 	NullNag,
 	GoodMove,
 	PoorMove,
@@ -181,200 +182,21 @@ typedef enum {
 	NagCount
 } Nag;
 
-const QString NagStrings[NagCount] =
-	{
-		"",
-		"!",
-		"?",
-		"!!",
-		"??",
-		"!?",
-		"?!",
-		"forced move",
-		"singular move",
-		"??",
-		"=",
-		"=, quiet position",
-		"=, active position",
-		"~",
-		"+=",
-		"=+",
-		"+/-",
-		"-/+",
-		"+-",
-		"-+",
-		"+-",
-		"-+",
-		"White is in zugzwang",
-		"Black is in zugzwang",
-		"White has a slight space advantage",
-		"Black has a slight space advantage",
-		"White has a moderate space advantage",
-		"Black has a moderate space advantage",
-		"White has a decisive space advantage",
-		"Black has a decisive space advantage",
-		"White has a slight time (development) advantage",
-		"Black has a slight time (development) advantage",
-		"White has a moderate time (development) advantage",
-		"Black has a moderate time (development) advantage",
-		"White has a decisive time (development) advantage",
-		"Black has a decisive time (development) advantage",
-		"White has the initiative",
-		"Black has the initiative",
-		"White has a lasting initiative",
-		"Black has a lasting initiative",
-		"White has the attack",
-		"Black has the attack",
-		"White has insufficient compensation for material deficit",
-		"Black has insufficient compensation for material deficit",
-		"~=",
-		"~=",
-		"White has more than adequate compensation for material deficit",
-		"Black has more than adequate compensation for material deficit",
-		"White has a slight center control advantage",
-		"Black has a slight center control advantage",
-		"White has a moderate center control advantage",
-		"Black has a moderate center control advantage",
-		"White has a decisive center control advantage",
-		"Black has a decisive center control advantage",
-		"White has a slight kingside control advantage",
-		"Black has a slight kingside control advantage",
-		"White has a moderate kingside control advantage",
-		"Black has a moderate kingside control advantage",
-		"White has a decisive kingside control advantage",
-		"Black has a decisive kingside control advantage",
-		"White has a slight queenside control advantage",
-		"Black has a slight queenside control advantage",
-		"White has a moderate queenside control advantage",
-		"Black has a moderate queenside control advantage",
-		"White has a decisive queenside control advantage",
-		"Black has a decisive queenside control advantage",
-		"White has a vulnerable first rank",
-		"Black has a vulnerable first rank",
-		"White has a well protected first rank",
-		"Black has a well protected first rank",
-		"White has a poorly protected king",
-		"Black has a poorly protected king",
-		"White has a well protected king",
-		"Black has a well protected king",
-		"White has a poorly placed king",
-		"Black has a poorly placed king",
-		"White has a well placed king",
-		"Black has a well placed king",
-		"White has a very weak pawn structure",
-		"Black has a very weak pawn structure",
-		"White has a moderately weak pawn structure",
-		"Black has a moderately weak pawn structure",
-		"White has a moderately strong pawn structure",
-		"Black has a moderately strong pawn structure",
-		"White has a very strong pawn structure",
-		"Black has a very strong pawn structure",
-		"White has poor knight placement",
-		"Black has poor knight placement",
-		"White has good knight placement",
-		"Black has good knight placement",
-		"White has poor bishop placement",
-		"Black has poor bishop placement",
-		"White has good bishop placement",
-		"Black has good bishop placement",
-		"White has poor rook placement",
-		"Black has poor rook placement",
-		"White has good rook placement",
-		"Black has good rook placement",
-		"White has poor queen placement",
-		"Black has poor queen placement",
-		"White has good queen placement",
-		"Black has good queen placement",
-		"White has poor piece coordination",
-		"Black has poor piece coordination",
-		"White has good piece coordination",
-		"Black has good piece coordination",
-		"White has played the opening very poorly",
-		"Black has played the opening very poorly",
-		"White has played the opening poorly",
-		"Black has played the opening poorly",
-		"White has played the opening well",
-		"Black has played the opening well",
-		"White has played the opening very well",
-		"Black has played the opening very well",
-		"White has played the middlegame very poorly",
-		"Black has played the middlegame very poorly",
-		"White has played the middlegame poorly",
-		"Black has played the middlegame poorly",
-		"White has played the middlegame well",
-		"Black has played the middlegame well",
-		"White has played the middlegame very well",
-		"Black has played the middlegame very well",
-		"White has played the ending very poorly",
-		"Black has played the ending very poorly",
-		"White has played the ending poorly",
-		"Black has played the ending poorly",
-		"White has played the ending well",
-		"Black has played the ending well",
-		"White has played the ending very well",
-		"Black has played the ending very well",
-		"White has slight counterplay",
-		"Black has slight counterplay",
-		"White has moderate counterplay",
-		"Black has moderate counterplay",
-		"White has decisive counterplay",
-		"Black has decisive counterplay",
-		"White has moderate time control pressure",
-		"Black has moderate time control pressure",
-		"White has severe time control pressure",
-		"Black has severe time control pressure",
-		"with the idea",
-		"aimed against",
-		"Better was",
-		"Worse was",
-		"Equivalent was",
-		"RR",
-		"N",
-		"weak point",
-		"endgame",
-		"line",
-		"diagonal",
-		"White has a pair of bishops",
-		"Black has a pair of bishops",
-		"Bishops of opposite color",
-		"Bishops of same color"
-	};
-
-const int MaxNags = 8;
-
 /** @ingroup Database
 The NagSet class stores set of PGN NAGs.
 */
-class NagSet
+class NagSet : public QVector<Nag>
 {
 public:
-	NagSet();
-	NagSet(Nag nag);
-	NagSet(const QVector<Nag>& nags);
-	NagSet& operator=(const NagSet& nagset);
-	Nag operator[](int index);
-	Nag operator[](int index) const;
+    enum { Simple, PGN }; /**< format of a string */
 
-	void addNag(Nag nag);
-	void removeNag(Nag nag);
-	void clear();
-	int count() const;
+    NagSet() {}
+    NagSet(Nag nag) { addNag(nag); }
 
-	QVector<Nag> nags() const;
-	QString toString() const;
-	QString toPGNString() const;
-
-	static QString nagToString(Nag nag);
-	static Nag stringToNag(const QString& nagString);
-
-private:
-	static void initialize();
-
-	quint8 m_noNags;
-	quint8 m_nags[MaxNags];
-
-	static bool initialized;
-	static QMap<QString, Nag> stringMap;
+    void addNag(Nag nag);
+    void removeNag(Nag nag);
+	QString toString(unsigned format = Simple) const;
+	static const QString& nagToString(Nag nag);
 };
 
 #endif	// __NAG_H__
