4d13d2f11378c3cd8095751c266ba92070521122
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2009-06-14 11:28:22 +0000

    Fix: keep focus in GameText when browsing game.

--- trunk/src/gui/mainwindow.cpp
+++ trunk/src/gui/mainwindow.cpp
@@ -745,6 +745,7 @@ void MainWindow::slotMoveChanged()
 			    .arg(header).arg(move).arg(var));
 	if (AppSettings->value("/General/onlineTablebases", true).toBool())
 		m_tablebase->getBestMove(g.toFen());
+	m_gameView->setFocus();
 
 	slotSearchTree();
 	emit boardChange(g.board());
