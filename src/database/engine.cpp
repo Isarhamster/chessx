a0262f0b4a4447770a88b1eda217982710dc8c45
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2009-10-12 09:29:55 +0000

    Skip obsolete disconnect.

--- src/database/engine.cpp
+++ src/database/engine.cpp
@@ -62,7 +62,6 @@ Engine::~Engine()
 	if (m_process) {
 		deactivate();
 		m_process->disconnect();
-		disconnect(m_process, SIGNAL(readyReadStandardOutput()),0,0);
 		m_process->kill();
 		m_process->waitForFinished(1000);
 		delete m_process;
