e052f73129bcceb24eac3f2aa36cff71f3248f17
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2010-02-08 23:30:43 +0000

    Preferences->Add engine now requires to select executable.

--- trunk/src/database/enginedata.cpp
+++ trunk/src/database/enginedata.cpp
@@ -7,8 +7,20 @@
  *   (at your option) any later version.                                   *
  ***************************************************************************/
 
+#include <QApplication>
 #include "enginedata.h"
 
 EngineData::EngineData(const QString& newName) : name(newName), protocol(UCI)
 {
+}
+
+
+QString EngineData::commandToName(const QString& command)
+{
+	QString name = command.section('/', -1, -1);
+	if (!name.isEmpty())
+		name[0] = name[0].toUpper();
+	if (name.endsWith(".exe"))
+		name.truncate(name.length() - 4);
+	return name.isEmpty() ? qApp->tr("New engine") : name;
 }
