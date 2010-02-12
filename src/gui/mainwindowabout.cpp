740c51a85c83cfac6d264599fc4880134cbba234
b_rizzuti <b_rizzuti@1de841de-8530-0410-b3ab-d372fa6ee214>
2010-02-12 21:47:31 +0000

    Update copyright date in the About window.

--- src/gui/mainwindowabout.cpp
+++ src/gui/mainwindowabout.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   (C) 2009 by Michal Rudolf mrudolf@kdewebdev.org                       *
+ *   (C) 2010 by Michal Rudolf mrudolf@kdewebdev.org                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -15,7 +15,7 @@ void MainWindow::slotHelpAbout()
 	QString fastbits = Board::fastbitsOption ? tr("Compiled with 'fastbits' option<br>") : "";
 	QMessageBox dialog(tr(""), tr("<h1>ChessX</h1>"
 						"<p>Free chess database available under GPLv2.<br>Version %1<br>%2"
-				      "Copyright 2005-2009 ChessX developers"
+				      "Copyright 2005-2010 ChessX developers"
 				      "<p>Current developer and maintainer: Michal Rudolf <a href=\"mailto:mrudolf"
 				      "@kdewebdev.org\">&lt;mrudolf"
 				      "@kdewebdev.org&gt;</a>"
