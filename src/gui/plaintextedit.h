8d5503edd383d4e27b2f645c5b4ac495a168bde8
mrudolf <mrudolf@1de841de-8530-0410-b3ab-d372fa6ee214>
2010-09-26 15:26:47 +0000

    Use Shift+Return for new line in comment. Use Return to close.

---
+++ src/gui/plaintextedit.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+*   Copyright (C) 2010 by Michal Rudolf <mrudolf@kdewebdev.org>           *
+****************************************************************************/
+
+#ifndef PLAINTEXTEDIT_H
+#define PLAINTEXTEDIT_H
+
+#include <QtGui>
+
+class PlainTextEdit : public QPlainTextEdit
+{
+public:
+	 PlainTextEdit(QWidget* parent = 0);
+protected:
+	 virtual void keyPressEvent(QKeyEvent* event);
+};
+
+#endif // PLAINTEXTEDIT_H
