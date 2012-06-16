2255331b86a8106bef76c1c3f0791142bacea8fa
hognose <14833289+Isarhamster@users.noreply.github.com>
2012-06-16 12:05:19 +0000

    Enable Debugging into file

---
+++ src/gui/logstream.cpp
@@ -0,0 +1,51 @@
+#include "logstream.h"
+#include <QDebug>
+#include <fstream>
+#include <QTime>
+#include <QDir>
+
+using namespace std;
+ofstream logfile;
+
+static bool bDoLog = false;
+void SimpleLoggingHandler(QtMsgType type, const char *msg)
+{
+    if (!bDoLog) return;
+
+    switch (type)
+    {
+        case QtDebugMsg:
+            logfile << QTime::currentTime().toString().toAscii().data() << " : "         << msg << endl;
+            break;
+        case QtCriticalMsg:
+            logfile << QTime::currentTime().toString().toAscii().data() << " Critical: " << msg << endl;
+            break;
+        case QtWarningMsg:
+            logfile << QTime::currentTime().toString().toAscii().data() << " Warning: "  << msg << endl;
+            break;
+        case QtFatalMsg:
+            logfile << QTime::currentTime().toString().toAscii().data() <<  " Fatal: "   << msg << endl;
+            break;
+     }
+}
+
+
+void startFileLog()
+{
+#ifdef _DEBUG
+    QString currentPath = QDir::currentPath()+ "/chessx.log";
+    logfile.open(currentPath.toAscii().data(),ios::app);
+    bDoLog = true;
+    qInstallMsgHandler(SimpleLoggingHandler);
+#endif
+}
+
+void stopFileLog()
+{
+    if (bDoLog)
+    {
+        bDoLog = false;
+        logfile.close();
+    }
+}
+
