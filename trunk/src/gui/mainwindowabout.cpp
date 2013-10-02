86978e1424f5912055c0adb3de778b698ca33bb8
hognose <hognose@1de841de-8530-0410-b3ab-d372fa6ee214>
2013-10-02 10:06:32 +0000

    New About Dialog

--- trunk/src/gui/mainwindowabout.cpp
+++ trunk/src/gui/mainwindowabout.cpp
@@ -7,34 +7,12 @@
  *   (at your option) any later version.                                   *
  ***************************************************************************/
 
+#include "aboutdlg.h"
 #include "mainwindow.h"
-#include "settings.h"
-#include "version.h"
-
-#include <QMessageBox>
-#include <QPixmap>
 
 void MainWindow::slotHelpAbout()
 {
-    QString version = QString(STR_REVISION).replace(',','.');
-    QMessageBox dlg(QMessageBox::NoIcon, tr("About ChessX"), tr("<h1>ChessX</h1>"
-                        "<p>Free chess database Version %1</p>"
-                        COPYRIGHT
-                        "<p>License: GNU Public License 2 - GPLv2</p>"
-                        "<p>Current maintainer: <a href=\"mailto:jens-chessx@gmx.net\">Jens Nissen</a></p>"
-                        "<p>Testing, usability and website: Bruno Rizzuti</p>"
-                        "<p>Further maintainers: Michal Rudolf, Ejner Borgbjerg</p>"
-                        "<p>Additional coding: James Coons, Marius Roets, Sean Estabrooks, Rico Zenklusen, "
-                        "Heinz Hopfgartner, William Hoggarth.</p>"
-                        "<p>Homepage: <a href=\"http://chessx.sourceforge.net\">http://chessx.sourceforge.net</a><br>"
-                        "Mailing list: <a href=\"mailto:chessx-users@lists.sourceforge.net\">chessx-users@lists.sourceforge.net</a><br>"
-                        "Temp Path: %2<br>"
-                        "AppData: %3<br></p>")
-                    .arg(version)
-                    .arg(AppSettings->getTempPath())
-                    .arg(AppSettings->dataPath()),
-                QMessageBox::Ok);
-    dlg.setIconPixmap((QPixmap(":/images/chessx.png")));
+    AboutDlg dlg;
     dlg.exec();
 }
 
