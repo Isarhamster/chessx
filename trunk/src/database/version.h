f762cf371c11facaf60394f644e15211d42320d4
hognose <hognose@1de841de-8530-0410-b3ab-d372fa6ee214>
2013-11-13 18:40:30 +0000

    Workaround for Apache Issue: #183 Download of PGN databases fails because of blocked user-agent

--- trunk/src/database/version.h
+++ trunk/src/database/version.h
@@ -17,8 +17,13 @@
 #define XGLUE4(a,b,c,d) #a "," #b "," #c "," #d
 #define GLUE4(a,b,c,d)  XGLUE4(a,b,c,d)
 
+#define XGLUEDOT3(a,b,c) #a "." #b "." #c
+#define GLUEDOT3(a,b,c)  XGLUEDOT3(a,b,c)
+
 #define STR_VERSION     GLUE2(VERSION_MAJOR,VERSION_MINOR)
 #define STR_REVISION    GLUE4(VERSION_MAJOR,VERSION_MINOR,REVISION,0)
+
+#define STR_VERSION_NET GLUEDOT3(VERSION_MAJOR,VERSION_MINOR,REVISION)
 
 #define COPYRIGHT_DATE "(C) 2013"
 #define COPYRIGHT "Copyright "##COPYRIGHT_DATE
