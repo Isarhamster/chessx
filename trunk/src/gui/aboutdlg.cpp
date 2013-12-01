fd59d40db697fcf440749d2882b5d7283ca5adbc
hognose <hognose@1de841de-8530-0410-b3ab-d372fa6ee214>
2013-12-01 19:15:31 +0000

    New feature: Show location of AppSettings in About Dlg

--- trunk/src/gui/aboutdlg.cpp
+++ trunk/src/gui/aboutdlg.cpp
@@ -13,16 +13,24 @@ AboutDlg::AboutDlg(QWidget *parent) :
     ui(new Ui::AboutDlg)
 {
     ui->setupUi(this);
+
     QString refPath = AppSettings->dataPath();
     ui->labelDataPath->setText(refPath);
+
     refPath = AppSettings->getTempPath();
     ui->labelTempPath->setText(refPath);
+
     ui->labelCopyRightDate->setText(COPYRIGHT_DATE);
+
     QString version = QString(STR_REVISION).replace(',', '.');
     ui->labelVersion->setText(version);
 
+    QString settingsPath = AppSettings->fileName();
+    ui->labelSettingsPath->setText(settingsPath);
+
     ui->labelTempPath->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
     ui->labelDataPath->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+    ui->labelSettingsPath->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
     ui->labelHomepage->setTextInteractionFlags(Qt::TextBrowserInteraction);
     ui->labelMailingList->setTextInteractionFlags(Qt::TextBrowserInteraction);
 }
