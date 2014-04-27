69a493e3b76aadd76243e09dbded5e353bf99748
hognose <hognose@1de841de-8530-0410-b3ab-d372fa6ee214>
2014-04-27 09:15:14 +0000

    Refactoring: Move Opening Tree Updater into own module

--- trunk/src/database/openingtree.h
+++ trunk/src/database/openingtree.h
@@ -14,55 +14,7 @@
 
 #include "filter.h"
 #include "game.h"
-
-class MoveData
-{
-public:
-    enum MoveType {StandardMove, GameEnd};
-    MoveData();
-    void addGame(Game& g, Color c, MoveType movetype = StandardMove);
-    double percentage() const;
-    int averageRating() const;
-    int averageYear() const;
-    QString move;
-    unsigned count;
-    unsigned result[4];
-    long rating;
-    unsigned rated;
-    long year;
-    unsigned dated;
-    friend bool operator<(const MoveData& m1, const MoveData& m2);
-    friend bool compareMove(const MoveData& m1, const MoveData& m2);
-    friend bool compareScore(const MoveData& m1, const MoveData& m2);
-    friend bool compareRating(const MoveData& m1, const MoveData& m2);
-    friend bool compareYear(const MoveData& m1, const MoveData& m2);
-};
-
-
-class OpeningTreeUpdater : public QThread
-{
-    Q_OBJECT
-public:
-    void run();
-    void cancel();
-    bool updateFilter(Filter& f, const Board& b, int&, bool updateFilter, bool sourceIsFilter, bool bEnd);
-
-signals:
-    void MoveUpdate(Board*, QList<MoveData>*);
-    void UpdateFinished(Board*);
-    void UpdateTerminated(Board*);
-    void progress(int);
-
-private:
-    int* m_games;
-
-    bool    m_break;
-    Board   m_board;
-    Filter* m_filter;
-    bool m_updateFilter;
-    bool m_sourceIsFilter;
-    bool m_bEnd;
-};
+#include "openingtreethread.h"
 
 /** @ingroup Search
 The OpeningTree class is a class to calculate opening tree for given position. */
---
+++ trunk/src/database/openingtreethread.cpp
@@ -0,0 +1,158 @@
+#include "openingtreethread.h"
+#include "database.h"
+
+MoveData::MoveData()
+{
+    count = 0;
+    for(int  r = ResultUnknown; r <= BlackWin; ++r)
+    {
+        result[r] = 0;
+    }
+    year = rating = 0;
+    dated = rated = 0;
+
+}
+
+void MoveData::addGame(Game& g, Color c, MoveType movetype)
+{
+    if(!count)
+        move = (movetype == StandardMove) ? g.moveToSan(Game::MoveOnly, Game::PreviousMove)
+               : qApp->translate("MoveData", "[end]");
+    ++count;
+    result[g.result()]++;
+    unsigned elo = (c == White) ? g.tag("WhiteElo").toInt() : g.tag("BlackElo").toInt();
+    if(elo >= 1000)
+    {
+        rating += elo;
+        ++rated;
+    }
+    unsigned y = g.tag("Date").section(".", 0, 0).toInt();
+    if(y > 1000)
+    {
+        year += y;
+        ++dated;
+    }
+}
+
+double MoveData::percentage() const
+{
+    unsigned c = result[ResultUnknown] + 2 * result[WhiteWin] + result[Draw];
+    return c * 500 / count / 10.0;
+}
+
+int MoveData::averageRating() const
+{
+    return rated ? rating / rated : 0;
+}
+
+int MoveData::averageYear() const
+{
+    return dated ? year / dated : 0;
+}
+
+bool operator<(const MoveData& m1, const MoveData& m2)
+{
+    return m1.count < m2.count || (m1.count == m2.count && m1.move < m2.move);
+}
+
+void OpeningTreeThread::run()
+{
+    Game g;
+    QMap<Move, MoveData> moves;
+    int games = 0;
+    QTime updateTime = QTime::currentTime().addSecs(1);
+    emit progress(0);
+    emit MoveUpdate(&m_board, new QList<MoveData>);
+    for(int i = 0; i < m_filter->size(); ++i)
+    {
+        if (m_sourceIsDatabase || m_filter->contains(i))
+        {
+            m_filter->database()->lock();
+            m_filter->database()->loadGameMoves(i, g);
+            m_filter->database()->unlock();
+            int id = g.findPosition(m_board);
+            if((id != NO_MOVE) && (m_bEnd ? g.atGameEnd(id) : true))
+            {
+                if(m_updateFilter)
+                {
+                    m_filter->set(i, id + 1); // not zero means success, but id could be 0.
+                }
+                m_filter->database()->lock();
+                m_filter->database()->loadGameHeaders(i, g);
+                m_filter->database()->unlock();
+                g.moveToId(id);
+                if(g.atGameEnd())
+                {
+                    moves[Move()].addGame(g, m_board.toMove(), MoveData::GameEnd);
+                }
+                else
+                {
+                    g.forward();
+                    moves[g.move()].addGame(g, m_board.toMove());
+                }
+                ++games;
+            }
+            else
+            {
+                if(m_updateFilter)
+                {
+                    m_filter->set(i, 0);
+                }
+            }
+        }
+
+        if (updateTime<=QTime::currentTime())
+        {
+            updateTime = QTime::currentTime().addSecs(3);
+            emit progress(i * 100 / m_filter->size());
+            if(!m_break)
+            {
+                *m_games = games;
+                QList<MoveData>* moveList = new QList<MoveData>();
+                for(QMap<Move, MoveData>::iterator it = moves.begin(); it != moves.end(); ++it)
+                {
+                    moveList->append(it.value());
+                }
+                emit MoveUpdate(&m_board, moveList);
+            }
+        }
+        if(m_break)
+        {
+            break;
+        }
+    }
+    *m_games = games;
+    if(!m_break)
+    {
+        QList<MoveData>* moveList = new QList<MoveData>();
+        for(QMap<Move, MoveData>::iterator it = moves.begin(); it != moves.end(); ++it)
+        {
+            moveList->append(it.value());
+        }
+        emit MoveUpdate(&m_board, moveList);
+        emit UpdateFinished(&m_board);
+    }
+    else
+    {
+        emit UpdateTerminated(&m_board);
+    }
+}
+
+void OpeningTreeThread::cancel()
+{
+    m_break = true;
+}
+
+bool OpeningTreeThread::updateFilter(Filter& f, const Board& b, int& g, bool updateFilter, bool sourceIsDatabase, bool bEnd)
+{
+    m_break = false;
+    m_filter = &f;
+    m_board = b;
+    m_games = &g;
+    m_bEnd  = bEnd;
+    m_updateFilter = updateFilter;
+    m_sourceIsDatabase = sourceIsDatabase;
+    // todo: if running wait for stop
+    start();
+    return true;
+}
