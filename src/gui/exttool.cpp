1a3cbe8a1f640421202de263968da43944ee25a8
hognose <14833289+Isarhamster@users.noreply.github.com>
2015-01-03 18:25:42 +0000

    Refactor from DataBaseList into separate module

---
+++ src/gui/exttool.cpp
@@ -0,0 +1,77 @@
+/****************************************************************************
+*   Copyright (C) 2012 by Jens Nissen jens-chessx@gmx.net                   *
+****************************************************************************/
+
+#include "exttool.h"
+#include "messagedialog.h"
+#include "settings.h"
+
+#include <QFileInfo>
+#include <QProcess>
+#include <QString>
+#include <QStringList>
+
+
+ExtTool::ExtTool(QObject *parent) : QObject(parent)
+{
+
+}
+
+ExtTool::~ExtTool()
+{
+
+}
+
+void ExtTool::extToolReadOutput()
+{
+    while(m_extToolProcess && m_extToolProcess->canReadLine())
+    {
+        QString message = m_extToolProcess->readLine().simplified();
+        errText.append(message);
+        errText.append("\n");
+    }
+}
+
+void ExtTool::RunExtTool1(QString pathIn)
+{
+    errText.clear();
+
+    QString extTool1 = AppSettings->value("Tools/Path1").toString();
+    QFileInfo fiExtTool(extTool1);
+    QFileInfo fiPathIn(pathIn);
+    m_extToolProcess = new QProcess(this);
+    if(fiExtTool.exists() && fiPathIn.exists() && m_extToolProcess)
+    {
+        m_extToolProcess->setReadChannel(QProcess::StandardError);
+        m_extToolProcess->setWorkingDirectory(fiPathIn.absolutePath());
+        connect(m_extToolProcess, SIGNAL(readyReadStandardOutput()), SLOT(extToolReadOutput()));
+        QStringList options;
+
+        options << fiExtTool.absoluteFilePath();
+        QString parameter = AppSettings->value("Tools/CommandLine1").toString();
+        parameter.replace("$(InputPath)",fiPathIn.absoluteFilePath());
+        parameter.replace("$(InputFile)",fiPathIn.fileName());
+        parameter.replace("$(InputDir)",fiPathIn.absolutePath());
+        parameter.replace("$(InputName)",fiPathIn.baseName());
+
+        options << parameter;
+        QString command = options.join(" ");
+        m_extToolProcess->start(command);
+        if (!m_extToolProcess->waitForFinished())
+        {
+            MessageDialog::warning(fiExtTool.baseName() + ": " + m_extToolProcess->errorString());
+        }
+        else
+        {
+            extToolReadOutput();
+            if (!errText.isEmpty())
+            {
+                MessageDialog::warning(fiExtTool.baseName() + ": " +errText);
+            }
+        }
+    }
+    else
+    {
+        MessageDialog::warning(fiExtTool.baseName() + tr(": File not found"));
+    }
+}
