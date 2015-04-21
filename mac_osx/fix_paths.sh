dac3c06a9cb733676a3e6b1bfc1d898a375dd66f
hognose <14833289+Isarhamster@users.noreply.github.com>
2015-04-21 07:59:38 +0000

    A bit housekeeping

---
+++ mac_osx/fix_paths.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+BIN_FILE="./chessx.app/Contents/MacOS/chessx"
+for P in `otool -L $BIN_FILE` 
+do 
+    if [[ "$P" == *//* ]] 
+    then 
+        PSLASH=$(echo $P | sed 's,//,/,g')
+        install_name_tool -change $P $PSLASH $BIN_FILE
+        echo 'LOG: install_name_tool -change $P $PSLASH $BIN_FILE'
+    fi 
+done 
+ 
+QTDIR="/Users/ni/Qt-5-3-0/5.3/clang_64"
+for F in `find $QTDIR/lib $QTDIR/plugins $QTDIR/qml  -perm 755 -type f` 
+do 
+    for P in `otool -L $F`
+	do   
+	    if [[ "$P" == *//* ]] 
+	    then 
+	        PSLASH=$(echo $P | sed 's,//,/,g')
+	        install_name_tool -change $P $PSLASH $F
+                echo 'LOG: install_name_tool -change $P $PSLASH $F'
+	    fi 
+	 done
+done
