35a8376083c7ea0ac93dafc6b102503b13a9e171
hognose <14833289+Isarhamster@users.noreply.github.com>
2015-09-26 13:37:17 +0000

    Bugfix: Openingthread crashes at end of FICS game

--- src/database/memorydatabase.h
+++ src/database/memorydatabase.h
@@ -11,6 +11,7 @@
 #ifndef __MEMORYDATABASE_H__
 #define __MEMORYDATABASE_H__
 
+#include <QMutex>
 #include <QVector>
 #include "pgndatabase.h"
 
@@ -63,6 +64,7 @@ private:
     QVector <Game*> m_games;
     bool parseFile();
     bool m_isModified;
+    QMutex m_mutex;
 };
 
 #endif	// __MEMORYDATABASE_H__
