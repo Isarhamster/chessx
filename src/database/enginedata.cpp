08255ad56e828902debf5a8608d1e5009d793485
hognose <14833289+Isarhamster@users.noreply.github.com>
2016-03-18 08:27:16 +0000

    Fix a warning

--- src/database/enginedata.cpp
+++ src/database/enginedata.cpp
@@ -14,7 +14,7 @@
 #define new DEBUG_NEW
 #endif // _MSC_VER
 
-EngineData::EngineData(const QString& newName) : name(newName), protocol(UCI), logging(false)
+EngineData::EngineData(const QString& newName) : name(newName), logging(false), protocol(UCI)
 {
 }
 
