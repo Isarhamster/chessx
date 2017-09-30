53f46189ae8b62ba93cc036232a965e64b7683c2
hognose <14833289+Isarhamster@users.noreply.github.com>
2017-09-30 15:46:21 +0000

    Forward translation flag in SAN generator

--- src/database/bitboard.h
+++ src/database/bitboard.h
@@ -124,7 +124,7 @@ public:
     /** Return a SAN string representation of given move */
     QString moveToSan(const Move& move, bool translate = false, bool extend = false) const;
     /** @return a SAN string representing a given move with move number. */
-    QString moveToFullSan(const Move& move) const;
+    QString moveToFullSan(const Move& move, bool translate=false) const;
 
     // Validation
     //
