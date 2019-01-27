81d7c36aac8b6401766e6ee1a88f419d3cd06118
hognose <hognose@1de841de-8530-0410-b3ab-d372fa6ee214>
2019-01-27 20:29:47 +0000

    Add generic font resizing to list widgets

---
+++ trunk/src/gui/listwidgetex.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************
+*   Copyright (C) 2019 by Jens Nissen jens-chessx@gmx.net                   *
+****************************************************************************/
+
+#include <QFont>
+#include <QObject>
+#include <QWheelEvent>
+#include "listwidgetex.h"
+
+ListWidgetEx::ListWidgetEx(QWidget*)
+{
+}
+
+bool ListWidgetEx::eventFilter(QObject *, QEvent *event)
+{
+    if (event->type() == QEvent::Wheel)
+    {
+        QWheelEvent *wEvent = dynamic_cast<QWheelEvent *>(event);
+        if (wEvent)
+        {
+            myWheelEvent(wEvent);
+        }
+    }
+    return false;
+}
+
+void ListWidgetEx::setFontSize(int fontSize)
+{
+    fontSize = std::max(fontSize, 8);
+    QFont f = font();
+    f.setPointSize(fontSize);
+    setFont(f);
+}
+
+void ListWidgetEx::myWheelEvent(QWheelEvent* e)
+{
+    unsigned int button = e->modifiers() & Qt::KeyboardModifierMask;
+    if (button == (Qt::ControlModifier|Qt::AltModifier))
+    {
+        QFont f = font();
+        int n = e->delta()>0 ? 1 : -1;
+        int fontSize = f.pointSize() + n;
+        setFontSize(fontSize);
+    }
+}
