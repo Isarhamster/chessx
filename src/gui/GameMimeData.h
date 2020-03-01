4d74f7933984df77a86cc0452b1b18a750554b52
hognose <14833289+Isarhamster@users.noreply.github.com>
2020-03-01 21:48:08 +0000

    Clarify mime data handling

--- src/gui/GameMimeData.h
+++ src/gui/GameMimeData.h
@@ -21,6 +21,14 @@ public:
 class DbMimeData : public QMimeData
 {
     Q_OBJECT
+#ifdef Q_OS_DARWIN
+public:
+    QList<QUrl> urls() const { return m_urls; }
+    void setUrls(const QList<QUrl> &urls) { m_urls = urls; }
+    bool hasUrls() const { return m_urls.count() > 0; }
+private:
+    QList<QUrl> m_urls;
+#endif
 };
 
 #endif // GAMEMIMEDATA_H
