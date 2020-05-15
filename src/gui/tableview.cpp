ac558b0ac1fcf90ec3662bc0f2a8a2420f4d1a04
Eugene Rysaj <eugene.rysaj@gmail.com>
2020-05-15 13:25:40 +0300

    Fix compiler warning: unused parameter ‘<param>’ [-Wunused-parameter]

--- src/gui/tableview.cpp
+++ src/gui/tableview.cpp
@@ -290,25 +290,30 @@ QImage TableView::renderToImage() const
 
 void TableView::startDrag(Qt::DropActions supportedActions)
 {
+    Q_UNUSED(supportedActions);
     m_dragTimer.stop();
 }
 
 void TableView::dragEnterEvent(QDragEnterEvent *event)
 {
+    Q_UNUSED(event);
     m_dragTimer.stop();
 }
 
 void TableView::dragMoveEvent(QDragMoveEvent *event)
 {
+    Q_UNUSED(event);
 }
 
 void TableView::dragLeaveEvent(QDragLeaveEvent *event)
 {
+    Q_UNUSED(event);
     m_dragTimer.start(250);
 }
 
 void TableView::dropEvent(QDropEvent *event)
 {
+    Q_UNUSED(event);
     m_dragTimer.stop();
 }
 
