cda987c00aa679ba601ae807a4bc4007cd6b38a8
Isarhamster <frodo.hobbit@gmx.net>
2020-10-23 09:31:21 +0200

    Fix icons in menus

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -93,12 +93,10 @@ prevHook = _CrtSetReportHook(customReportHook);
 // _CrtSetBreakAlloc(157); // Use this line to break at the nth memory allocation
 #endif
 
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);     // HiDPI support
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);     // HiDPI support
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);  // Windows Surface Book
 
     QApplication app(argc, argv);
-
-    app.setAttribute(Qt::AA_DontShowIconsInMenus);  // Icons are *no longer shown* in menus
 
     QDir dir(QApplication::applicationDirPath());
 
@@ -139,6 +137,9 @@ prevHook = _CrtSetReportHook(customReportHook);
     {
         AppSettings = new Settings;
     }
+
+    bool showIcons = AppSettings->getValue("/MainWindow/ShowMenuIcons").toBool();
+    if (!showIcons) app.setAttribute(Qt::AA_DontShowIconsInMenus);  // Icons are *no longer shown* in menus
 
     // style our application with custom dark style
     Style* chessxStyle = new Style;
