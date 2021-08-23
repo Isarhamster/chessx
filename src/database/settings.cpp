ca429bec6cdede0cd93488b7fd8aaa95fc6308a1
Israel Hernandez <israel.a.hernandez@gmail.com>
2021-08-22 19:07:57 -0700

    Change default Theme to Adwaita

--- src/database/settings.cpp
+++ src/database/settings.cpp
@@ -256,7 +256,7 @@ QMap<QString, QVariant> Settings::initDefaultValues() const
     map.insert("/MainWindow/GameToolBar", false);
     map.insert("/MainWindow/VerticalTabs", false);
     map.insert("/MainWindow/DarkTheme", false);
-    map.insert("/MainWindow/Theme", "plastique");
+    map.insert("/MainWindow/Theme", "Adwaita");
     map.insert("/MainWindow/StayOnTop", false);
     map.insert("/MainWindow/FilterFollowsGame", false);
     map.insert("/MainWindow/ShowMenuIcons", true);
